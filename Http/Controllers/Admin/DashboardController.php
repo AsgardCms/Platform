fc846dd08f8ab77fd5bd08fab88c2104e35aa5a2
Nicolas Widart <n.widart@gmail.com>
2014-09-22 21:24:51 +0200

    Adding filters for admin dashboard

--- Http/Controllers/Admin/DashboardController.php
+++ Http/Controllers/Admin/DashboardController.php
@@ -1,9 +1,9 @@
 <?php namespace Modules\Dashboard\Http\Controllers\Admin;
 
-use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\View;
+use Modules\Core\Http\Controllers\Admin\AdminBaseController;
 
-class DashboardController extends Controller
+class DashboardController extends AdminBaseController
 {
     public function index()
     {
