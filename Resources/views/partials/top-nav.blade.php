6fb444dddeda9ce85f82e7ab7362e8e9e4abad10
Nicolas Widart <n.widart@gmail.com>
2014-09-22 17:23:12 +0200

    Adding logout link

--- Resources/views/partials/top-nav.blade.php
+++ Resources/views/partials/top-nav.blade.php
@@ -34,7 +34,7 @@
                             <a href="#" class="btn btn-default btn-flat">Profile</a>
                         </div>
                         <div class="pull-right">
-                            <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                            <a href="{{ URL::route('logout')  }}" class="btn btn-default btn-flat">Sign out</a>
                         </div>
                     </li>
                 </ul>
