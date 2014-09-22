db7d73d0a96cfc762effd1b2a2806c54368c341a
Nicolas Widart <n.widart@gmail.com>
2014-09-22 15:21:34 +0200

    Removing the sidebar link from core module

--- Composers/SidebarViewCreator.php
+++ Composers/SidebarViewCreator.php
@@ -9,12 +9,5 @@ class SidebarViewCreator
     {
         $view->prefix = Config::get('core::core.admin-prefix');
         $view->items = new Collection;
-        $view->items->put('dashboard', [
-                'weight' => 0,
-                'request' => $view->prefix,
-                'route' => 'dashboard.index',
-                'icon-class' => 'fa fa-dashboard',
-                'title' => 'Dashboard',
-            ]);
     }
 }
