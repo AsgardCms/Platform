a87b58301fca292b97ec543c7b2998539a4d7c9d
Nicolas Widart <n.widart@gmail.com>
2014-09-23 21:47:11 +0200

    Add test to secure the create and edit views

--- Tests/UrlTest.php
+++ Tests/UrlTest.php
@@ -5,17 +5,34 @@ use Illuminate\Support\Facades\Config;
 
 class UrlTest extends \TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        $user = Sentinel::findById(4);
+        Sentinel::login($user);
+    }
+
     /** @test */
     public function userIndexShouldBeAccessible()
     {
-        $user = Sentinel::findById(4);
-        Sentinel::login($user);
-
         $uri = '/' . Config::get('core::core.admin-prefix') . '/users';
         $crawler = $this->client->request('GET', $uri);
 
         $this->assertTrue($this->client->getResponse()->isOk());
 
         $this->assertCount(1, $crawler->filter('h1:contains("Users")'));
+    }
+
+
+    /** @test */
+    public function userEditShouldBeAccessible()
+    {
+        $uri = '/' . Config::get('core::core.admin-prefix') . '/users/4/edit';
+        $crawler = $this->client->request('GET', $uri);
+
+        $this->assertTrue($this->client->getResponse()->isOk());
+
+        $this->assertCount(1, $crawler->filter('h1:contains("Edit User")'));
     }
 }
