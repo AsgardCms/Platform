48b02cd4b953562438162a4da3d488f7118b574a
Nicolas Widart <n.widart@gmail.com>
2014-09-23 21:37:34 +0200

    Adding Users & roles submenu

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -1,15 +1,31 @@
 <?php namespace Modules\User\Composers;
 
+use Illuminate\Support\Collection;
+
 class SidebarViewComposer
 {
     public function compose($view)
     {
-        $view->items->put('user', [
-            'weight' => 1,
-            'request' => "{$view->prefix}/users",
-            'route' => 'dashboard.user.index',
-            'icon-class' => 'fa fa-user',
-            'title' => 'Users',
-        ]);
+        $view->items->put('user', Collection::make([
+            [
+                'weight' => '1',
+                'request' => "{$view->prefix}/users*",
+                'route' => '#',
+                'icon-class' => 'fa fa-user',
+                'title' => 'Users & Roles',
+            ],
+            [
+                'request' => "{$view->prefix}/users",
+                'route' => 'dashboard.user.index',
+                'icon-class' => 'fa fa-user',
+                'title' => 'Users',
+            ],
+            [
+                'request' => "{$view->prefix}/messages/roles",
+                'route' => 'dashboard.user.index',
+                'icon-class' => 'fa fa-flag-o',
+                'title' => 'Roles',
+            ]
+        ]));
     }
 }
