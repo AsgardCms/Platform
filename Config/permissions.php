bbd05bdb110ef7eb9e06d105814c3efce5980ca4
Nicolas Widart <n.widart@gmail.com>
2014-09-26 13:34:58 +0200

    Adding more permissions

--- Config/permissions.php
+++ Config/permissions.php
@@ -3,13 +3,17 @@ return [
     'users' => [
         'index',
         'create',
+        'store',
         'edit',
+        'update',
         'delete'
     ],
     'roles' => [
         'index',
         'create',
+        'store',
         'edit',
+        'update',
         'delete'
     ],
 ];
