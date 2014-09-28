b0c820a7c506674026274da663130b507d35111a
Nicolas Widart <n.widart@gmail.com>
2014-09-28 17:12:31 +0200

    File organisation

--- Http/routes.php
+++ Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Dashboard\Http\Controllers'], function()
+Route::group(['prefix' => Config::get('core::config.admin-prefix'), 'namespace' => 'Modules\Dashboard\Http\Controllers'], function()
 {
 	Route::get('/', ['as' => 'dashboard.index', 'uses' => 'Admin\DashboardController@index']);
 });
