5e558f47a433f60b30cefb842433c56afa138c04
Nicolas Widart <n.widart@gmail.com>
2014-09-29 20:39:55 +0200

    Setting the browser lang for testing env

--- Http/routes.php
+++ Http/routes.php
@@ -5,6 +5,9 @@
 | Language Settings
 |--------------------------------------------------------------------------
 */
+if (App::environment() == 'testing') {
+    $_SERVER['HTTP_ACCEPT_LANGUAGE'] = 'fr_FR';
+}
 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 App::setLocale('fr');
