9dc1f336a7d15c828b0f7ee251e5ef3f41378d0c
Nicolas Widart <n.widart@gmail.com>
2014-10-03 09:33:09 +0200

    Modifying the prefix

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -6,7 +6,7 @@ class SidebarViewComposer
     {
         $view->items->put('dashboard', [
             'weight' => 0,
-            'request' => $view->prefix,
+            'request' => "*/$view->prefix",
             'route' => 'dashboard.index',
             'icon-class' => 'fa fa-dashboard',
             'title' => 'Dashboard',
