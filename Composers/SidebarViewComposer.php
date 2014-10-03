c341d8dd96468956daf0271a50a4e6523d49be98
Nicolas Widart <n.widart@gmail.com>
2014-10-03 09:34:22 +0200

    Modifying the navigation for i18n

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -10,19 +10,19 @@ class SidebarViewComposer
         $view->items->put('user', Collection::make([
             [
                 'weight' => '1',
-                'request' => Request::is("{$view->prefix}/users*") or Request::is("{$view->prefix}/roles*"),
+                'request' => Request::is("*/{$view->prefix}/users*") or Request::is("*/{$view->prefix}/roles*"),
                 'route' => '#',
                 'icon-class' => 'fa fa-user',
                 'title' => 'Users & Roles',
             ],
             [
-                'request' => "{$view->prefix}/users*",
+                'request' => "*/{$view->prefix}/users*",
                 'route' => 'dashboard.user.index',
                 'icon-class' => 'fa fa-user',
                 'title' => 'Users',
             ],
             [
-                'request' => "{$view->prefix}/roles*",
+                'request' => "*/{$view->prefix}/roles*",
                 'route' => 'dashboard.role.index',
                 'icon-class' => 'fa fa-flag-o',
                 'title' => 'Roles',
