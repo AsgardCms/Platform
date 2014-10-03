340fdb950a0f6079cfbd019c45fc006adda3d908
Nicolas Widart <n.widart@gmail.com>
2014-10-03 09:26:26 +0200

    Add a route filter for i18n

--- Http/routes.php
+++ Http/routes.php
@@ -1,22 +1,24 @@
 <?php
 
-$locale = LaravelLocalization::setLocale();
-Route::group(['prefix' => $locale . '/' . Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\User\Http\Controllers'], function()
+Route::group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter'], function()
 {
-    Route::resource('users', 'Admin\UserController', ['except' => ['show'], 'names' => [
-            'index' => 'dashboard.user.index',
-            'create' => 'dashboard.user.create',
-            'store' => 'dashboard.user.store',
-            'edit' => 'dashboard.user.edit',
-            'update' => 'dashboard.user.update',
-            'destroy' => 'dashboard.user.destroy',
+    Route::group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\User\Http\Controllers'], function()
+    {
+        Route::resource('users', 'Admin\UserController', ['except' => ['show'], 'names' => [
+                'index' => 'dashboard.user.index',
+                'create' => 'dashboard.user.create',
+                'store' => 'dashboard.user.store',
+                'edit' => 'dashboard.user.edit',
+                'update' => 'dashboard.user.update',
+                'destroy' => 'dashboard.user.destroy',
+            ]]);
+        Route::resource('roles', 'Admin\RolesController', ['except' => ['show'], 'names' => [
+            'index' => 'dashboard.role.index',
+            'create' => 'dashboard.role.create',
+            'store' => 'dashboard.role.store',
+            'edit' => 'dashboard.role.edit',
+            'update' => 'dashboard.role.update',
+            'destroy' => 'dashboard.role.destroy',
         ]]);
-    Route::resource('roles', 'Admin\RolesController', ['except' => ['show'], 'names' => [
-        'index' => 'dashboard.role.index',
-        'create' => 'dashboard.role.create',
-        'store' => 'dashboard.role.store',
-        'edit' => 'dashboard.role.edit',
-        'update' => 'dashboard.role.update',
-        'destroy' => 'dashboard.role.destroy',
-    ]]);
+    });
 });
