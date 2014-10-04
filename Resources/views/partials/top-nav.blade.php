836f24196ea550610292fff41cc6a31702174e6e
Nicolas Widart <n.widart@gmail.com>
2014-10-04 15:17:40 +0200

    Add a language selector

--- Resources/views/partials/top-nav.blade.php
+++ Resources/views/partials/top-nav.blade.php
@@ -8,7 +8,26 @@
         <span class="icon-bar"></span>
     </a>
     <div class="navbar-right">
+
         <ul class="nav navbar-nav">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-flag"></i>
+                    <span>
+                        {{ LaravelLocalization::getCurrentLocaleName()  }}
+                        <i class="caret"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu language-menu">
+                    @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                        <li {{ App::getLocale() == $localeCode ? ' class="active"' : '' }}>
+                            <a rel="alternate" hreflang="{{$localeCode}}" href="{{LaravelLocalization::getLocalizedURL($localeCode) }}">
+                                {{{ $properties['native'] }}}
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
             <!-- User Account: style can be found in dropdown.less -->
             <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
