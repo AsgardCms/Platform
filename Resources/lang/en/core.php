2162e5f02aec2e0497125174fdc1094261f396cf
Nicolas Widart <n.widart@gmail.com>
2014-10-04 20:52:33 +0200

    Adding general translation keys

--- Resources/lang/en/core.php
+++ Resources/lang/en/core.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-
+    'modal' => [
+        'title' => 'Confirmation',
+        'confirmation-message' => 'Are you sure you want to delete this record?'
+    ],
+    'table' => [
+        'created at' => 'Created at',
+        'actions' => 'Actions',
+    ],
+    'button' => [
+        'cancel' => 'Cancel',
+        'update' => 'Update',
+        'delete' => 'Delete',
+    ],
+    'breadcrumb' => [
+        'home' => 'Home'
+    ]
 ];
---
+++ Resources/lang/fr/core.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'modal' => [
+        'title' => 'Confirmation',
+        'confirmation-message' => 'Etes vous sur de vouloir supprimer cette resource?'
+    ],
+    'table' => [
+        'created at' => 'Crée à',
+        'actions' => 'Actions',
+    ],
+    'button' => [
+        'cancel' => 'Annuler',
+        'update' => 'Mise à jour',
+        'delete' => 'Suprimer',
+    ],
+    'breadcrumb' => [
+        'home' => 'Accueil'
+    ]
+];
