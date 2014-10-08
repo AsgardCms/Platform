91743084a7a29efa4dda7d6a875e86e7a6f29fcc
Abdel Nalbou <abdel@MacBook-Pro-de-Abdel.local>
2014-10-08 14:03:47 +0200

    added PHPdoc

--- Repositories/CoreRepository.php
+++ Repositories/CoreRepository.php
@@ -1,13 +1,32 @@
 <?php namespace Modules\Core\Repositories;
 
+/**
+ * Interface CoreRepository
+ * @package Modules\Core\Repositories
+ */
 interface CoreRepository
 {
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function find($id);
 
+    /**
+     * @return mixed
+     */
     public function all();
 
+    /**
+     * @param $data
+     * @return mixed
+     */
     public function add($data);
 
+    /**
+     * @param $ids
+     * @return mixed
+     */
     public function remove($ids);
 
 }
--- Repositories/Eloquent/EloquentCoreRepository.php
+++ Repositories/Eloquent/EloquentCoreRepository.php
@@ -3,30 +3,55 @@
 use Illuminate\Database\Eloquent\Model;
 use Modules\Core\Repositories\CoreRepository;
 
+/**
+ * Class EloquentCoreRepository
+ * @package Modules\Core\Repositories\Eloquent
+ */
 abstract class EloquentCoreRepository implements CoreRepository
 {
+    /**
+     * @var Model An instance of the Eloquent Model
+     */
     protected $model;
 
+    /**
+     * @param Model $model
+     */
     public function __construct(Model $model)
     {
         $this->model = $model;
     }
 
+    /**
+     * @param int $id
+     * @return mixed
+     */
     public function find($id)
     {
         return $this->model->find($id);
     }
 
+    /**
+     * @return mixed
+     */
     public function all()
     {
         return $this->model->all();
     }
 
+    /**
+     * @param mixed $data
+     * @return mixed
+     */
     public function add($data)
     {
         return $this->model->create($data);
     }
 
+    /**
+     * @param int|int[] $ids
+     * @return mixed
+     */
     public function remove($ids)
     {
         return $this->model->destroy($ids);
