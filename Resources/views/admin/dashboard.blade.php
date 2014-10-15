dac8bd33e3ccbdc3edcb1fbb08891e598319b201
Nicolas Widart <n.widart@gmail.com>
2014-10-15 21:28:50 +0200

    Squashed 'Modules/Dashboard/' changes from 26e5069..7eef842
    7eef842 Adding the translated breadcrumb
    c82ef0a Adding flash notifications on dashboard
    
    git-subtree-dir: Modules/Dashboard
    git-subtree-split: 7eef842a2ef0f0e2e8d0c6ba95c3931097579a84

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -5,13 +5,14 @@
     Dashboard
 </h1>
 <ol class="breadcrumb">
-    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li><a href="#"><i class="fa fa-dashboard"></i> {{ trans('core::core.breadcrumb.home') }}</a></li>
 </ol>
 @stop
 
 @section('content')
 <div class="row">
     <div class="col-md-12">
+        @include('flash::message')
         <p>{{ trans('dashboard::dashboard.welcome-message') }}</p>
     </div>
 </div>
