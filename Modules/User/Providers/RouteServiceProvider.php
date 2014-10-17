4b5e86eddb7a84a9401a2b9b105ce7792d572ccd
Nicolas Widart <n.widart@gmail.com>
2014-10-17 12:06:10 +0200

    New routes SP structure.

--- Modules/User/Providers/RouteServiceProvider.php
+++ Modules/User/Providers/RouteServiceProvider.php
@@ -1,38 +1,48 @@
 <?php namespace Modules\User\Providers;
 
 use Illuminate\Routing\Router;
-use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
-	/**
-	 * Called before routes are registered.
-	 *
-	 * Register any model bindings or pattern based filters.
-	 *
-	 * @param  Router  $router
-	 * @param  UrlGenerator  $url
-	 * @return void
-	 */
-	public function before(Router $router, UrlGenerator $url)
-	{
-		$url->setRootControllerNamespace('Modules\User\Http\Controllers');
-	}
+    /**
+     * The root namespace to assume when generating URLs to actions.
+     *
+     * @var string
+     */
+    protected $rootUrlNamespace = 'Modules\User\Http\Controllers';
 
-	/**
-	 * Define the routes for the application.
-	 *
-	 * @return void
-	 */
-	public function map()
-	{
-		$this->app->booted(function()
-		{
-			$this->namespaced('Modules\User\Http\Controllers', function(Router $router)
-			{
-				require __DIR__ . '/../Http/routes.php';
-			});
-		});
-	}
+    /**
+     * The controllers to scan for route annotations.
+     *
+     * @var array
+     */
+    protected $scan = [
+        'Modules\User\Http\Controllers',
+    ];
+
+    /**
+     * Called before routes are registered.
+     *
+     * Register any model bindings or pattern based filters.
+     *
+     * @param  Router $router
+     * @return void
+     */
+    public function before(Router $router)
+    {
+        //
+    }
+
+    /**
+     * Define the routes for the application.
+     *
+     * @return void
+     */
+    public function map(Router $router)
+    {
+        require __DIR__ . '/../Http/routes.php';
+    }
+
 }
+
