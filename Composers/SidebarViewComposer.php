6bb24243edbd83255e74aaf4b4a9b4214bbc751d
Nicolas Widart <n.widart@gmail.com>
2014-10-18 18:12:20 +0200

    Type hinting view class

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Request;
+use Illuminate\View\View;
 
 class SidebarViewComposer
 {
-    public function compose($view)
+    public function compose(View $view)
     {
         $view->items->put('setting', [
             'weight' => 5,
