254c526d0594a49c761af6e4198ab9520d276ccd
Nicolas Widart <n.widart@gmail.com>
2014-10-18 15:05:11 +0200

    Squashed 'Modules/Workshop/' changes from a6cc96b..31cd758
    31cd758 switching asset function
    
    git-subtree-dir: Modules/Workshop
    git-subtree-split: 31cd7584c460a4a6cf8558ef8cfc9ce217598a9d

--- Resources/views/admin/workbench/index.blade.php
+++ Resources/views/admin/workbench/index.blade.php
@@ -11,7 +11,7 @@
 @stop
 
 @section('styles')
-<link href="{{{ core_asset('css/vendor/iCheck/flat/blue.css') }}}" rel="stylesheet" type="text/css" />
+<link href="{{{ Module::asset('core', 'css/vendor/iCheck/flat/blue.css') }}}" rel="stylesheet" type="text/css" />
 @stop
 
 @section('content')
