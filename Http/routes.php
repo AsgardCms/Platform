fee719903127c10061724dc3d28234d2fd3bdeee
Nicolas Widart <n.widart@gmail.com>
2014-10-19 19:41:16 +0200

    Remove unused routes

--- Http/routes.php
+++ Http/routes.php
@@ -3,13 +3,9 @@
 $router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin'], function($router) {
 	$router->group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Setting\Http\Controllers'], function($router)
 	{
-		$router->resource('settings', 'Admin\SettingController', ['except' => ['show'], 'names' => [
+		$router->resource('settings', 'Admin\SettingController', ['except' => ['show', 'edit', 'update', 'destroy'], 'names' => [
 			'index' => 'dashboard.setting.index',
-			'create' => 'dashboard.setting.create',
-			'store' => 'dashboard.setting.store',
-			'edit' => 'dashboard.setting.edit',
-			'update' => 'dashboard.setting.update',
-			'destroy' => 'dashboard.setting.destroy',
+			'store' => 'dashboard.setting.store'
 		]]);
 	});
 });
