42501b0e2e21689f0038f37e6a75db1e624650c4
Nicolas Widart <n.widart@gmail.com>
2014-10-20 07:59:23 +0200

    Preparing for settings with custom input type

--- Resources/views/admin/partials/module-fields.blade.php
+++ Resources/views/admin/partials/module-fields.blade.php
@@ -1,11 +1,11 @@
-<?php foreach($moduleSettings as $setting => $description): ?>
+<?php foreach($moduleSettings as $setting => $moduleInfo): ?>
     <?php $settingName = $module . '_' . $setting; ?>
     <div class='form-group'>
-        {!! Form::label($settingName . "[$lang]", $description) !!}
+        {!! Form::label($settingName . "[$lang]", $moduleInfo['description']) !!}
         <?php if (isset($settings[$settingName])): ?>
-            {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]", $settings[$settingName]->translate($lang)->value), ['class' => 'form-control', 'placeholder' => $description]) !!}
+            {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]", $settings[$settingName]->translate($lang)->value), ['class' => 'form-control', 'placeholder' => $moduleInfo['description']]) !!}
         <?php else: ?>
-            {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => $description]) !!}
+            {!! Form::text($settingName . "[$lang]", Input::old($settingName . "[$lang]"), ['class' => 'form-control', 'placeholder' => $moduleInfo['description']]) !!}
         <?php endif; ?>
     </div>
 <?php endforeach; ?>
