267637ef981a4948a5e907a6c7fc7d453f53de07
Nicolas Widart <n.widart@gmail.com>
2014-10-26 10:34:03 +0100

    Squashed 'Modules/Workshop/' changes from 31cd758..a484d0e
    a484d0e Type hinting the View contract
    b31b187 Changing namespace
    2c285a3 Adding permission key
    
    git-subtree-dir: Modules/Workshop
    git-subtree-split: a484d0e5ce0d80b4046eafb83ee1466e5f5864a4

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -1,11 +1,13 @@
 <?php namespace Modules\Workshop\Composers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Request;
+use Modules\Core\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
-    public function compose($view)
+    public function compose(View $view)
     {
         $view->items->put('workbench', Collection::make([
             [
@@ -14,18 +16,21 @@ class SidebarViewComposer
                 'route' => '#',
                 'icon-class' => 'fa fa-cogs',
                 'title' => 'Workshop',
+                'permission' => $this->auth->hasAccess('modules.index') or $this->auth->hasAccess('workbench.index')
             ],
             [
                 'request' => "*/{$view->prefix}/modules*",
                 'route' => 'dashboard.modules.index',
                 'icon-class' => 'fa fa-cog',
                 'title' => 'Modules',
+                'permission' => $this->auth->hasAccess('modules.index')
             ],
             [
                 'request' => "*/{$view->prefix}/workbench*",
                 'route' => 'dashboard.workbench.index',
                 'icon-class' => 'fa fa-terminal',
                 'title' => 'Workbench',
+                'permission' => $this->auth->hasAccess('workbench.index')
             ]
         ]));
     }
