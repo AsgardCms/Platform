5fa98e55eccaa76e20c632d0473bca4a7ffaf284
Nicolas Widart <n.widart@gmail.com>
2014-10-26 21:15:31 +0100

    Adding migrations

---
+++ Database/Migrations/2014_10_26_162751_create_files_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('files', function(Blueprint $table)
+		{
+            $table->increments('id');
+		    $table->string('filename');
+		    $table->string('path');
+		    $table->string('extension');
+		    $table->string('mimetype');
+		    $table->string('width');
+		    $table->string('height');
+		    $table->string('filesize');
+		    $table->string('folder_id')->unsigned();
+            $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('files');
+	}
+
+}
