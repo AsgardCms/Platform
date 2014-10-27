bd498e1a3c0ed8d18f32206dfeca7d3c957ed167
Nicolas Widart <n.widart@gmail.com>
2014-10-27 07:29:23 +0100

    Adding correct routes file

--- Http/routes.php
+++ Http/routes.php
@@ -1,8 +1,16 @@
 <?php
 
-Route::group(
-    ['prefix' => 'media', 'namespace' => 'Modules\Media\Http\Controllers'],
-    function () {
-        Route::get('/', 'MediaController@index');
-    }
-);
+$router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin'], function($router)
+    {
+        $router->group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Media\Http\Controllers'], function($router)
+            {
+                $router->resource('media', 'Admin\MediaController', ['except' => ['show'], 'names' => [
+                        'index' => 'dashboard.media.index',
+                        'create' => 'dashboard.media.create',
+                        'store' => 'dashboard.media.store',
+                        'edit' => 'dashboard.media.edit',
+                        'update' => 'dashboard.media.update',
+                        'destroy' => 'dashboard.media.destroy',
+                    ]]);
+            });
+    });
