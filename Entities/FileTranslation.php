8dfddc7f7c270f2f0c0452119a1ee2e46ea9f79d
Nicolas Widart <n.widart@gmail.com>
2014-10-27 17:03:13 +0100

    Adding basic entities

---
+++ Entities/FileTranslation.php
@@ -0,0 +1,9 @@
+<?php namespace Modules\Media\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FileTranslation extends Model
+{
+    public $timestamps = false;
+    protected $fillable = ['description', 'alt_attribute', 'keywords'];
+}
