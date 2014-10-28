d22ae2a6697ff1f1ce48601fd95176d3952dc974
Nicolas Widart <n.widart@gmail.com>
2014-10-28 20:54:24 +0100

    Fixing the routes, adding permissions filter

--- Http/routes.php
+++ Http/routes.php
@@ -1,16 +1,22 @@
 <?php
 
-$router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin'], function($router)
+$router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin|permissions'], function($router)
     {
         $router->group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Media\Http\Controllers'], function($router)
             {
-                $router->resource('media', 'Admin\MediaController', ['only' => ['store'], 'names' => [
+                $router->resource('media', 'Admin\MediaController', ['except' => ['show'], 'names' => [
+                        'index' => 'dashboard.media.index',
+                        'create' => 'dashboard.media.create',
                         'store' => 'dashboard.media.store',
+                        'edit' => 'dashboard.media.edit',
+                        'update' => 'dashboard.media.update',
+                        'destroy' => 'dashboard.media.destroy',
                     ]]);
             });
     });
 
-$router->group(['prefix' => 'api', 'namespace' => 'Modules\Media\Http\Controllers'], function ($router) {
-        $router->resource('media', 'Api\MediaController');
-    });
+$router->get('admin/grid-files', 'Modules\Media\Http\Controllers\Admin\MediaController@gridFiles');
 
+$router->group(['prefix' => 'api', 'namespace' => 'Modules\Media\Http\Controllers'], function ($router) {
+        $router->resource('media', 'Api\MediaController', ['only' => ['store']]);
+    });
