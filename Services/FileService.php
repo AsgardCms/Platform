3b2d51c320a2b92372bc3fe0895765598b423bde
Nicolas Widart <n.widart@gmail.com>
2014-10-28 13:49:21 +0100

    Adding a FileService

---
+++ Services/FileService.php
@@ -0,0 +1,29 @@
+<?php namespace Modules\Media\Services;
+
+use Modules\Media\Helpers\FileHelper;
+use Modules\Media\Repositories\FileRepository;
+
+class FileService
+{
+    /**
+     * @var FileRepository
+     */
+    private $file;
+
+    public function __construct(FileRepository $file)
+    {
+        $this->file = $file;
+    }
+
+    public function store($file)
+    {
+        // Save the file info to db
+        $savedFile = $this->file->createFromFile($file);
+        dd($savedFile);
+        // Move the uploaded file to /public/assets/media/
+        $file->move(public_path() . '/assets/media', $savedFile->path);
+
+        return $savedFile;
+    }
+
+}
