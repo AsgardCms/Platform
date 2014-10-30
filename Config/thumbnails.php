978fa91374353c7709cf820daee7f93622a9ce4e
Nicolas Widart <n.widart@gmail.com>
2014-10-30 15:37:05 +0100

    Removing comments from media setting

--- Config/thumbnails.php
+++ Config/thumbnails.php
@@ -2,70 +2,9 @@
 
 return [
     'smallThumb' => [
-
-//        'crop' => [
-//            'width' => '100',
-//            'height' => '200'
-//        ],
-//        'fit' => [
-//            'width' => '100',
-//            'height' => '200',
-//            'position' => 'top-left'
-//        ],
-//        'blur' => [
-//            'amount' => '15'
-//        ],
-//        'brightness' => [
-//            'level' => '50'
-//        ],
-//        'colorize' => [
-//            'red' => 0,
-//            'green' => 0,
-//            'blue' => 99
-//        ],
-//        'contrast' => [
-//            'level' => 65
-//        ],
-//        'flip' => [
-//            'mode' => 'h'
-//        ],
-//        'gamma' => [
-//            'correction' => '1.8'
-//        ],
-//         'greyscale' => [],
-//         'heighten' => [
-//             'height' => 500,
-//             'callback' => function($constraint) {
-//                 $constraint->upsize();
-//             }
-//         ]
-//           'invert' => [],
-//        'limitColors' => [
-//            'count' => 90,
-//            'matte' => '#ff9900'
-//        ],
-//        'opacity' => [
-//            'transparency' => 10
-//        ],
-//        'orientate' => [],
-//        'pixelate' => [
-//            'size' => 12
-//        ],
-//        'resize' => [
-//            'width' => 500,
-//            'height' => 100,
-//        ],
-//        'rotate' => [
-//            'angle' => -45
-//        ],
-//        'sharpen' => [
-//            'amount' => 30
-//        ],
-//        'trim' => [
-//            'base' => 'top-left',
-//        ],
-        'widen' => [
-            'width' => 1500
+        'crop' => [
+            'width' => '100',
+            'height' => '200'
         ],
     ]
 ];
