ad5489d5e9d449d9288d64486b33fd4a37e560b6
Nicolas Widart <n.widart@gmail.com>
2014-10-31 11:04:49 +0100

    Abstracting the destination file in test

--- Tests/ImagyTest.php
+++ Tests/ImagyTest.php
@@ -16,41 +16,54 @@ class ImagyTest extends BaseTestCase
      * @var \Illuminate\Filesystem\Filesystem
      */
     protected $finder;
+    /**
+     * @var \Illuminate\Contracts\Config\Repository
+     */
+    protected $config;
+    /**
+     * @var string
+     */
+    protected $mediaPath;
 
+    /**
+     *
+     */
     public function setUp()
     {
         parent::setUp();
-        $config = App::make('Illuminate\Contracts\Config\Repository');
+        $this->config = App::make('Illuminate\Contracts\Config\Repository');
         $module = App::make('Pingpong\Modules\Module');
         $this->finder = App::make('Illuminate\Filesystem\Filesystem');
-        $this->imagy = new Imagy(new InterventionFactory, new ThumbnailsManager($config, $module), $config);
+        $this->imagy = new Imagy(new InterventionFactory, new ThumbnailsManager($this->config, $module), $this->config);
+
+        $this->mediaPath = $this->config->get('media::config.files-path');
     }
 
     /** @test */
     public function it_should_create_a_file()
     {
-        if ($this->finder->isFile(public_path() . '/assets/media/google-map_smallThumb.png')) {
-            $this->finder->delete(public_path() . '/assets/media/google-map_smallThumb.png');
+        if ($this->finder->isFile(public_path() . "{$this->mediaPath}google-map_smallThumb.png")) {
+            $this->finder->delete(public_path() . "{$this->mediaPath}google-map_smallThumb.png");
         }
 
-        $this->imagy->get('/assets/media/google-map.png', 'smallThumb', true);
+        $this->imagy->get("{$this->mediaPath}google-map.png", 'smallThumb', true);
 
-        $this->assertTrue($this->finder->isFile(public_path() . '/assets/media/google-map_smallThumb.png'));
+        $this->assertTrue($this->finder->isFile(public_path() . "{$this->mediaPath}google-map_smallThumb.png"));
     }
 
     /** @test */
     public function it_should_not_create_thumbs_for_pdf_files()
     {
-        $this->imagy->get('/assets/media/test-pdf.pdf', 'smallThumb', true);
+        $this->imagy->get("{$this->mediaPath}test-pdf.pdf", 'smallThumb', true);
 
-        $this->assertFalse($this->finder->isFile(public_path() . '/assets/media/test-pdf_smallThumb.png'));
+        $this->assertFalse($this->finder->isFile(public_path() . "{$this->mediaPath}test-pdf_smallThumb.png"));
     }
 
     /** @test */
     public function it_should_return_thumbnail_path()
     {
-        $path = $this->imagy->getThumbnail('/assets/media/google-map.png', 'smallThumb');
-        $expected = '/assets/media/google-map_smallThumb.png';
+        $path = $this->imagy->getThumbnail("{$this->mediaPath}google-map.png", 'smallThumb');
+        $expected = "{$this->mediaPath}google-map_smallThumb.png";
 
         $this->assertEquals($expected, $path);
     }
@@ -58,8 +71,8 @@ class ImagyTest extends BaseTestCase
     /** @test */
     public function it_should_return_same_path_for_non_images()
     {
-        $path = $this->imagy->getThumbnail('/assets/media/test-pdf.pdf', 'smallThumb');
-        $expected = '/assets/media/test-pdf.pdf';
+        $path = $this->imagy->getThumbnail("{$this->mediaPath}test-pdf.pdf", 'smallThumb');
+        $expected = "{$this->mediaPath}test-pdf.pdf";
 
         $this->assertEquals($expected, $path);
     }
