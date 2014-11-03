bc5754d52e75a55b625506caa2fe3d2a9abcf925
Nicolas Widart <n.widart@gmail.com>
2014-11-03 13:46:18 +0100

    Typehinting the router

--- Http/routes.php
+++ Http/routes.php
@@ -1,24 +1,26 @@
 <?php
 
+use Illuminate\Routing\Router;
+
 $router->model('media', 'Modules\Media\Entities\File');
 
-$router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin|permissions'], function($router)
+$router->group(['prefix' => LaravelLocalization::setLocale(), 'before' => 'LaravelLocalizationRedirectFilter|auth.admin|permissions'], function(Router $router)
+{
+    $router->group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Media\Http\Controllers'], function(Router $router)
     {
-        $router->group(['prefix' => Config::get('core::core.admin-prefix'), 'namespace' => 'Modules\Media\Http\Controllers'], function($router)
-            {
-                $router->resource('media', 'Admin\MediaController', ['except' => ['show'], 'names' => [
-                        'index' => 'dashboard.media.index',
-                        'create' => 'dashboard.media.create',
-                        'store' => 'dashboard.media.store',
-                        'edit' => 'dashboard.media.edit',
-                        'update' => 'dashboard.media.update',
-                        'destroy' => 'dashboard.media.destroy',
-                    ]]);
-            });
+        $router->resource('media', 'Admin\MediaController', ['except' => ['show'], 'names' => [
+                'index' => 'dashboard.media.index',
+                'create' => 'dashboard.media.create',
+                'store' => 'dashboard.media.store',
+                'edit' => 'dashboard.media.edit',
+                'update' => 'dashboard.media.update',
+                'destroy' => 'dashboard.media.destroy',
+            ]]);
     });
+});
 
 $router->get('admin/grid-files', 'Modules\Media\Http\Controllers\Admin\MediaController@gridFiles');
 
-$router->group(['prefix' => 'api', 'namespace' => 'Modules\Media\Http\Controllers'], function ($router) {
-        $router->resource('media', 'Api\MediaController', ['only' => ['store']]);
-    });
+$router->group(['prefix' => 'api', 'namespace' => 'Modules\Media\Http\Controllers'], function (Router $router) {
+    $router->resource('media', 'Api\MediaController', ['only' => ['store']]);
+});
