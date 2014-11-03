07a5874bfb1139895eea11aaa75cad59b1bbc7a3
Nicolas Widart <n.widart@gmail.com>
2014-11-03 17:07:07 +0100

    Adding menus migrations

---
+++ Database/Migrations/2014_11_03_160015_create_menus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('menus');
+    }
+
+}
