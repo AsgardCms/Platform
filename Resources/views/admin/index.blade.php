0c33c626c7b07c844613ac17854b8d9282333435
Nicolas Widart <n.widart@gmail.com>
2014-11-04 12:42:23 +0100

    Adding nestable

--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -10,10 +10,41 @@
 </ol>
 @stop
 
+@section('styles')
+<link href="{!! Module::asset('menu', 'css/nestable.css') !!}" rel="stylesheet" type="text/css" />
+@stop
+
 @section('content')
 <div class="row">
     <div class="col-md-12">
-
+        <div class="dd">
+            <ol class="dd-list">
+                <li class="dd-item" data-id="1">
+                    <div class="dd-handle">Item 1</div>
+                </li>
+                <li class="dd-item" data-id="2">
+                    <div class="dd-handle">Item 2</div>
+                </li>
+                <li class="dd-item" data-id="3">
+                    <div class="dd-handle">Item 3</div>
+                    <ol class="dd-list">
+                        <li class="dd-item" data-id="4">
+                            <div class="dd-handle">Item 4</div>
+                        </li>
+                        <li class="dd-item" data-id="5">
+                            <div class="dd-handle">Item 5</div>
+                        </li>
+                    </ol>
+                </li>
+            </ol>
+        </div>
     </div>
 </div>
+@stop
+
+@section('scripts')
+<script src="{!! Module::asset('menu', 'js/jquery.nestable.js') !!}"></script>
+<script>
+$('.dd').nestable();
+</script>
 @stop
