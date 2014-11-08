7b8864da088a9ed146f7b2f862c5fc9090eefda7
Nicolas Widart <n.widart@gmail.com>
2014-11-08 20:29:24 +0100

    Using the new datatables syntax

--- Resources/views/admin/menus/index.blade.php
+++ Resources/views/admin/menus/index.blade.php
@@ -102,19 +102,20 @@
 <script type="text/javascript">
     $(function () {
         $('.data-table').dataTable({
-            "bPaginate": true,
-            "bLengthChange": true,
-            "bFilter": true,
-            "bSort": true,
-            "bInfo": true,
-            "bAutoWidth": true,
-            "oLanguage": {
-                "sUrl": '<?php echo Module::asset('core', "js/vendor/datatables/{$locale}.json") ?>'
+            "paginate": true,
+            "lengthChange": true,
+            "filter": true,
+            "sort": true,
+            "info": true,
+            "autoWidth": true,
+            "order": [[ 0, "asc" ]],
+            "language": {
+                "url": '<?php echo Module::asset('core', "js/vendor/datatables/{$locale}.json") ?>'
             },
-            "aoColumns": [
+            "columns": [
                 null,
                 null,
-                { "bSortable": false }
+                { "sortable": false }
             ]
         });
     });
