838c1ec76148845d77c3985fcd9e891bc8d3693b
Nicolas Widart <n.widart@gmail.com>
2014-11-14 11:02:35 +0100

    Fixing typo in interface name

--- Repositories/Eloquent/EloquentMenuItemRepository.php
+++ Repositories/Eloquent/EloquentMenuItemRepository.php
@@ -1,8 +1,25 @@
 <?php namespace Modules\Menu\Repositories\Eloquent;
 
 use Modules\Core\Repositories\Eloquent\EloquentBaseRepository;
-use Modules\Menu\Repositories\MenuRepository;
+use Modules\Menu\Entities\Menuitem;
+use Modules\Menu\Repositories\MenuItemRepository;
 
-class EloquentMenuItemRepository extends EloquentBaseRepository implements MenuRepository
+class EloquentMenuItemRepository extends EloquentBaseRepository implements MenuItemRepository
 {
+    public function create($data)
+    {
+        dd($data);
+        $menuItem = new Menuitem;
+        $menuItem->menu_id = $data['menu_id'];
+        $menuItem->page_id = $data['page_id'];
+        $menuItem->target = $data['target'];
+        $menuItem->module_name = $data['module_name'];
+
+        unset($data['menu_id'], $data['page_id'], $data['target'], $data['module_name']);
+
+        $translatableData = Helper::separateLanguages($data);
+        Helper::updateTranslated($menuItem, $translatableData);
+
+        return $menuItem;
+    }
 }
