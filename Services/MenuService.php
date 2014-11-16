a75482292db082afb28dd19ebb3138861386daa5
Nicolas Widart <n.widart@gmail.com>
2014-11-16 21:57:23 +0100

    Save child items recursively

--- Services/MenuService.php
+++ Services/MenuService.php
@@ -5,32 +5,59 @@ use Modules\Menu\Repositories\MenuItemRepository;
 class MenuService
 {
     /**
+     * Current Menu Item being looped over
+     * @var
+     */
+    protected $menuItem;
+    /**
      * @var MenuItemRepository
      */
-    private $menuItem;
+    private $menuItemRepository;
 
     public function __construct(MenuItemRepository $menuItem)
     {
-        $this->menuItem = $menuItem;
+        $this->menuItemRepository = $menuItem;
     }
 
     public function handle($item, $position)
     {
         // Find menu item : $menuItem['id'] ->setRoot
-        $menuItem = $this->menuItem->find($item['id']);
-        $menuItem->position = $position;
-        $menuItem->save();
-        $menuItem->makeRoot();
+        $this->menuItem = $this->menuItemRepository->find($item['id']);
+        $this->savePosition($this->menuItem, $position);
+        $this->menuItem->makeRoot();
 
         // If hasChildren ? set parent ˆ (recursive)
-        if (isset($item['children'])) {
-            foreach ($item['children'] as $childPosition => $childItem) {
-                $childMenuItem = $this->menuItem->find($childItem['id']);
-                $childMenuItem->position = $childPosition;
-                $childMenuItem->save();
-                $childMenuItem->makeChildOf($menuItem);
-            }
+        if ($this->hasChildren($item)) {
+            $this->setChildrenRecursively($item, $this->menuItem);
         }
-        // If no more children finish the recursive call
+    }
+
+    /**
+     * @param $item
+     * @return bool
+     */
+    private function hasChildren($item)
+    {
+        return isset($item['children']);
+    }
+
+    private function setChildrenRecursively($item, $parent)
+    {
+        foreach ($item['children'] as $childPosition => $childItem) {
+            $childMenuItem = $this->menuItemRepository->find($childItem['id']);
+            $this->savePosition($childMenuItem, $childPosition);
+            $childMenuItem->makeChildOf($parent);
+            if ($this->hasChildren($childItem)) $this->setChildrenRecursively($childItem, $childMenuItem);
+        }
+    }
+
+    /**
+     * @param $item
+     * @param $position
+     */
+    private function savePosition($item, $position)
+    {
+        $item->position = $position;
+        $item->save();
     }
 }
