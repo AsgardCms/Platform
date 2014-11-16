7a8aedff15ec48d5415ab3b1b2b8eb99a79ce7af
Nicolas Widart <n.widart@gmail.com>
2014-11-16 22:46:12 +0100

    Removing unused class property

--- Http/Controllers/Api/MenuItemController.php
+++ Http/Controllers/Api/MenuItemController.php
@@ -1,23 +1,17 @@
 <?php namespace Modules\Menu\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use Modules\Menu\Repositories\MenuItemRepository;
 use Modules\Menu\Services\MenuService;
 
 class MenuItemController
 {
     /**
-     * @var MenuItemRepository
-     */
-    private $menuItem;
-    /**
      * @var MenuService
      */
     private $menuService;
 
-    public function __construct(MenuItemRepository $menuItem, MenuService $menuService)
+    public function __construct(MenuService $menuService)
     {
-        $this->menuItem = $menuItem;
         $this->menuService = $menuService;
     }
 
