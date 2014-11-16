34dfe4e8c2234a3fb5521649295a4ff2cc0c93ab
Nicolas Widart <n.widart@gmail.com>
2014-11-16 22:33:46 +0100

    Refactor for clarity

--- Services/MenuRenderer.php
+++ Services/MenuRenderer.php
@@ -4,33 +4,35 @@ use Illuminate\Support\Facades\URL;
 
 class MenuRenderer
 {
+    protected $menuId;
     private $startTag = '<div class="dd">';
     private $endTag = '</div>';
     private $menu = '';
 
     public function renderForMenu($menuId, $menuItems)
     {
+        $this->menuId = $menuId;
+
         $this->menu .= $this->startTag;
-
-        $this->renderItemsForMenu($menuId, $menuItems);
-
+        $this->renderItems($menuItems);
         $this->menu .= $this->endTag;
 
         return $this->menu;
     }
 
-    private function renderItemsForMenu($menuId, $items)
+    private function renderItems($items)
     {
         $this->menu .= '<ol class="dd-list">';
         foreach ($items as $item) {
             $this->menu .= "<li class=\"dd-item\" data-id=\"{$item->id}\">";
             $this->menu .= '<a class="btn btn-sm btn-info"
                                    style="float:left; margin-right: 15px;"
-                                   href="' . URL::route('dashboard.menuitem.edit', [$menuId, $item->id]) . '">Edit</a>';
+                                   href="' . URL::route('dashboard.menuitem.edit', [$this->menuId, $item->id]) . '">
+                                   Edit</a>';
             $this->menu .= "<div class=\"dd-handle\">{$item->title}</div>";
 
             if (!$item->children()->get()->isEmpty()) {
-                $this->renderItemsForMenu($menuId, $item->children()->get());
+                $this->renderItems($item->children()->get());
             }
 
             $this->menu .= '</li>';
