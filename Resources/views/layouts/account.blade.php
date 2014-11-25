a10a0c1b79467f40f1f16407d3d9695c539d7f4a
Nicolas Widart <n.widart@gmail.com>
2014-11-25 11:57:47 +0100

    Squashed 'Modules/User/' changes from a241ed9..9b064da
    9b064da Use the new module assets method signature
    
    git-subtree-dir: Modules/User
    git-subtree-split: 9b064da37ba7611be9b810c324c86c4bca7d0683

--- Resources/views/layouts/account.blade.php
+++ Resources/views/layouts/account.blade.php
@@ -9,17 +9,17 @@
     </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- bootstrap 3.0.2 -->
-    <link href="{{{ Module::asset('core', 'css/vendor/bootstrap.min.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/vendor/bootstrap.min.css') }}}" rel="stylesheet" type="text/css" />
     <!-- font Awesome -->
-    <link href="{{{ Module::asset('core', 'css/vendor/font-awesome.min.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/vendor/font-awesome.min.css') }}}" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
-    <link href="{{{ Module::asset('core', 'css/vendor/ionicons.min.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/vendor/ionicons.min.css') }}}" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="{{{ Module::asset('core', 'css/AdminLTE.css') }}}" rel="stylesheet" type="text/css" />
-    <link href="{{{ Module::asset('core', 'css/vendor/alertify/alertify.core.css') }}}" rel="stylesheet" type="text/css" />
-    <link href="{{{ Module::asset('core', 'css/vendor/alertify/alertify.default.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/AdminLTE.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/vendor/alertify/alertify.core.css') }}}" rel="stylesheet" type="text/css" />
+    <link href="{{{ Module::asset('core:css/vendor/alertify/alertify.default.css') }}}" rel="stylesheet" type="text/css" />
 
-    <script src="{{{ Module::asset('core', 'js/vendor/jquery.min.js') }}}"></script>
+    <script src="{{{ Module::asset('core:js/vendor/jquery.min.js') }}}"></script>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -34,7 +34,7 @@
 </div>
 
 <!-- Bootstrap -->
-<script src="{{{ Module::asset('core', 'js/vendor/bootstrap.min.js') }}}" type="text/javascript"></script>
-<script src="{{{ Module::asset('core', 'js/vendor/alertify/alertify.js') }}}" type="text/javascript"></script>
+<script src="{{{ Module::asset('core:js/vendor/bootstrap.min.js') }}}" type="text/javascript"></script>
+<script src="{{{ Module::asset('core:js/vendor/alertify/alertify.js') }}}" type="text/javascript"></script>
 </body>
 </html>
