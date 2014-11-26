caa33367d43504ccc05abcf3d32986c66c60fdfa
Nicolas Widart <n.widart@gmail.com>
2014-11-26 10:32:08 +0100

    Squashed 'Modules/Dashboard/' changes from c64f10d..d4a122f
    d4a122f remove view namespace definitions from start file
    93c4d69 Adding start.php to files array
    
    git-subtree-dir: Modules/Dashboard
    git-subtree-split: d4a122face8ff9f40279a2c94ff962f767a5d9dd

--- start.php
+++ start.php
@@ -1,20 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Register The Module Namespaces
-|--------------------------------------------------------------------------
-|
-| Here is you can register the namespace for this module.
-| You may to edit this namespace if you want.
-|
-*/
-
-View::addNamespace('dashboard', __DIR__ . '/Resources/views/');
-
-Lang::addNamespace('dashboard', __DIR__ . '/Resources/lang/');
-
-Config::addNamespace('dashboard', __DIR__ . '/Config/');
-
-
 require __DIR__ . '/composers.php';
