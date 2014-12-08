21ab3ed7afc81209bf84de6326e281c77036e57e
Nicolas Widart <n.widart@gmail.com>
2014-12-08 19:58:47 +0100

    Removing the workshop module

--- Modules/Workshop/composers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-View::composer('core::partials.sidebar-nav', 'Modules\Workshop\Composers\SidebarViewComposer');
-View::composer([
-        'workshop::admin.workbench.tabs.migrate',
-        'workshop::admin.workbench.tabs.seed',
-    ], 'Modules\Workshop\Composers\MigrateViewComposer');
