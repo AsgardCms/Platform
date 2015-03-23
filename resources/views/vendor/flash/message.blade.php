557b02d5e4e8fc13f856852bb4644990c9646740
Nicolas Widart <n.widart@gmail.com>
2015-03-23 20:13:10 +0100

    Adding custom messages

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -9,3 +9,17 @@
         </div>
     @endif
 @endif
+
+@if (Session::has('success'))
+    <div class="alert alert-success">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('success') }}
+    </div>
+@endif
+
+@if (Session::has('error'))
+    <div class="alert alert-error">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        {{ Session::get('error') }}
+    </div>
+@endif
