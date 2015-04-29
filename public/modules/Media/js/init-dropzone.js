4f65e6d23a6083387cda6a3fcfae3103f9f73ae6
Nicolas Widart <n.widart@gmail.com>
2015-04-29 17:17:43 +0200

    Updating dependencies

---
+++ public/modules/Media/js/init-dropzone.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    Dropzone.autoDiscover = false;
+    var myDropzone = new Dropzone(".dropzone", {
+        url: '/api/file',
+        autoProcessQueue: true,
+        maxFilesize: maxFilesize,
+        acceptedFiles : acceptedFiles
+    });
+    myDropzone.on("success", function(file, http) {
+        window.setTimeout(function(){
+            location.reload();
+        }, 1000);
+    });
+    myDropzone.on("sending", function(file, fromData) {
+        if ($('.alert-danger').length > 0) {
+            $('.alert-danger').remove();
+        }
+    });
+    myDropzone.on("error", function(file, errorMessage) {
+        var html = '<div class="alert alert-danger" role="alert">' + errorMessage.error + '</div>';
+        $('.col-md-12').first().prepend(html);
+        setTimeout(function() {
+            myDropzone.removeFile(file);
+        }, 2000);
+    });
+});
