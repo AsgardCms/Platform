a4d0100a2c54a38788d9a70b9e61d6c8ba6da51c
Nicolas Widart <n.widart@gmail.com>
2015-05-04 16:35:49 +0200

    Update workshop published permissions

--- config/asgard.workshop.permissions.php
+++ config/asgard.workshop.permissions.php
@@ -3,25 +3,8 @@
 return [
     'workshop.modules' => [
         'index',
-        'store',
-    ],
-    'workshop.workbench' => [
-        'index',
-        'generate',
-        'migrate',
-        'install',
-        'seed',
-    ],
-    'workshop.generate' => [
-        'generate',
-    ],
-    'workshop.install' => [
-        'install',
-    ],
-    'workshop.migrate' => [
-        'migrate',
-    ],
-    'workshop.seed' => [
-        'seed',
+        'show',
+        'disable',
+        'enable',
     ],
 ];
