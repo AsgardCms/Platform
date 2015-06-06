f9368faae8cf584ca4a0a88a1c431703a8785cdb
Nicolas Widart <n.widart@gmail.com>
2015-06-06 12:38:32 +0200

    Set new migrations path

--- config/modules.php
+++ config/modules.php
@@ -64,7 +64,7 @@ return [
         |
         */
 
-        'migration' => app_path('database/migrations'),
+        'migration' => base_path('database/migrations'),
         /*
         |--------------------------------------------------------------------------
         | Generator path
