fd364b17248a1812f50580d44828c92e63527ac6
Nicolas Widart <n.widart@gmail.com>
2015-07-01 17:52:47 +0200

    Publish new user config

--- config/asgard.user.users.php
+++ config/asgard.user.users.php
@@ -1,7 +1,26 @@
 <?php
 
 return [
-
+    /*
+    |--------------------------------------------------------------------------
+    | Define which user driver to use.
+    |--------------------------------------------------------------------------
+    | Current default and only option : Sentinel
+    | Sentry option is outdated
+    */
     'driver' => 'Sentinel',
-
+    /*
+    |--------------------------------------------------------------------------
+    | Define which route to redirect to after a successful login
+    |--------------------------------------------------------------------------
+    */
+    'redirect_route_after_login' => 'homepage',
+    /*
+    |--------------------------------------------------------------------------
+    | Login column(s)
+    |--------------------------------------------------------------------------
+    | Define which column(s) you'd like to use to login with, currently
+    | only supported by the Sentinel user driver
+    */
+    'login-columns' => ['email'],
 ];
