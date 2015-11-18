c2fff7c79b7df11576a4f73828284e6ec754b53b
Nicolas Widart <n.widart@gmail.com>
2015-11-18 21:40:51 +0100

    Publishing asgard core config file

--- config/asgard.core.core.php
+++ config/asgard.core.core.php
@@ -54,6 +54,7 @@ return [
         'daterangepicker.css' => ['theme' => 'vendor/admin-lte/plugins/daterangepicker/daterangepicker-bs3.css'],
         'selectize.css' => ['module' => 'core:vendor/selectize/dist/css/selectize.css'],
         'selectize-default.css' => ['module' => 'core:vendor/selectize/dist/css/selectize.default.css'],
+        'animate.css' => ['theme' => 'vendor/animate.css/animate.min.css'],
         // Javascript
         'jquery.js' => ['theme' => 'vendor/jquery/dist/jquery.min.js'],
         'bootstrap.js' => ['theme' => 'vendor/bootstrap/dist/js/bootstrap.min.js'],
@@ -96,6 +97,7 @@ return [
            'dataTables.bootstrap.css',
            'AdminLTE.css',
            'AdminLTE.all.skins.css',
+           'animate.css',
            'asgard.css',
        ],
        'js' => [
