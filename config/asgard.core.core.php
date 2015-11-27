be89773b1e71737d1af93f63eeef4787415608aa
Nicolas Widart <n.widart@gmail.com>
2015-11-27 17:10:00 +0100

    Updating configuration

--- config/asgard.core.core.php
+++ config/asgard.core.core.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-   /*
-   |--------------------------------------------------------------------------
-   | The prefix that'll be used for the administration
-   |--------------------------------------------------------------------------
-   */
+    /*
+    |--------------------------------------------------------------------------
+    | The prefix that'll be used for the administration
+    |--------------------------------------------------------------------------
+    */
     'admin-prefix' => 'backend',
 
     /*
@@ -22,29 +22,48 @@ return [
     */
     'admin-theme' => 'AdminLTE',
 
-   /*
-   |--------------------------------------------------------------------------
-   | AdminLTE skin
-   |--------------------------------------------------------------------------
-   | You can customize the AdminLTE colors with this setting. The following
-   | colors are available for you to use: skin-blue, skin-green,
-   | skin-black, skin-purple, skin-red and skin-yellow.
-   */
-   'skin' => 'skin-blue',
+    /*
+    |--------------------------------------------------------------------------
+    | AdminLTE skin
+    |--------------------------------------------------------------------------
+    | You can customize the AdminLTE colors with this setting. The following
+    | colors are available for you to use: skin-blue, skin-green,
+    | skin-black, skin-purple, skin-red and skin-yellow.
+    */
+    'skin' => 'skin-blue',
 
-   /*
-   |--------------------------------------------------------------------------
-   | Define which assets will be available through the asset manager
-   |--------------------------------------------------------------------------
-   | These assets are registered on the asset manager
-   */
+    /*
+    |--------------------------------------------------------------------------
+    | Middleware
+    |--------------------------------------------------------------------------
+    | You can customise the Middleware that should be loaded.
+    | The localizationRedirect middleware is automatically loaded for both
+    | Backend and Frontend routes.
+    */
+    'middleware' => [
+        'backend' => [
+            'auth.admin',
+            'permissions',
+        ],
+        'frontend' => [
+        ],
+        'api' => [
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Define which assets will be available through the asset manager
+    |--------------------------------------------------------------------------
+    | These assets are registered on the asset manager
+    */
     'admin-assets' => [
         // Css
         'bootstrap.css' => ['theme' => 'vendor/bootstrap/dist/css/bootstrap.min.css'],
         'font-awesome.css' => ['theme' => 'vendor/font-awesome/css/font-awesome.min.css'],
         'alertify.core.css' => ['theme' => 'css/vendor/alertify/alertify.core.css'],
         'alertify.default.css' => ['theme' => 'css/vendor/alertify/alertify.default.css'],
-        'dataTables.bootstrap.css' => ['theme' => 'css/vendor/datatables/dataTables.bootstrap.css'],
+        'dataTables.bootstrap.css' => ['theme' => 'vendor/datatables.net-bs/css/dataTables.bootstrap.min.css'],
         'icheck.blue.css' => ['theme' => 'vendor/iCheck/skins/flat/blue.css'],
         'AdminLTE.css' => ['theme' => 'vendor/admin-lte/dist/css/AdminLTE.css'],
         'AdminLTE.all.skins.css' => ['theme' => 'vendor/admin-lte/dist/css/skins/_all-skins.min.css'],
@@ -56,13 +75,12 @@ return [
         'selectize-default.css' => ['module' => 'core:vendor/selectize/dist/css/selectize.default.css'],
         'animate.css' => ['theme' => 'vendor/animate.css/animate.min.css'],
         // Javascript
-        'jquery.js' => ['theme' => 'vendor/jquery/dist/jquery.min.js'],
         'bootstrap.js' => ['theme' => 'vendor/bootstrap/dist/js/bootstrap.min.js'],
         'mousetrap.js' => ['theme' => 'js/vendor/mousetrap.min.js'],
         'alertify.js' => ['theme' => 'js/vendor/alertify/alertify.js'],
         'icheck.js' => ['theme' => 'vendor/iCheck/icheck.min.js'],
-        'jquery.dataTables.js' => ['theme' => 'js/vendor/datatables/jquery.dataTables.js'],
-        'dataTables.bootstrap.js' => ['theme' => 'js/vendor/datatables/dataTables.bootstrap.js'],
+        'jquery.dataTables.js' => ['theme' => 'vendor/datatables.net/js/jquery.dataTables.min.js'],
+        'dataTables.bootstrap.js' => ['theme' => 'vendor/datatables.net-bs/js/dataTables.bootstrap.min.js'],
         'jquery.slug.js' => ['theme' => 'js/vendor/jquery.slug.js'],
         'app.js' => ['theme' => 'vendor/admin-lte/dist/js/app.js'],
         'keypressAction.js' => ['module' => 'core:js/keypressAction.js'],
@@ -82,37 +100,37 @@ return [
         'chart.js' => ['theme' => 'vendor/admin-lte/plugins/chartjs/Chart.js'],
     ],
 
-   /*
-   |--------------------------------------------------------------------------
-   | Define which default assets will always be included in your pages
-   | through the asset pipeline
-   |--------------------------------------------------------------------------
-   */
-   'admin-required-assets' => [
-       'css' => [
-           'bootstrap.css',
-           'font-awesome.css',
-           'alertify.core.css',
-           'alertify.default.css',
-           'dataTables.bootstrap.css',
-           'AdminLTE.css',
-           'AdminLTE.all.skins.css',
-           'animate.css',
-           'asgard.css',
-       ],
-       'js' => [
-           'jquery.js',
-           'bootstrap.js',
-           'mousetrap.js',
-           'alertify.js',
-           'icheck.js',
-           'jquery.dataTables.js',
-           'dataTables.bootstrap.js',
-           'jquery.slug.js',
-           'keypressAction.js',
-           'app.js',
-           'main.js',
-           'sisyphus.js',
-       ],
-   ],
+    /*
+    |--------------------------------------------------------------------------
+    | Define which default assets will always be included in your pages
+    | through the asset pipeline
+    |--------------------------------------------------------------------------
+    */
+    'admin-required-assets' => [
+        'css' => [
+            'bootstrap.css',
+            'font-awesome.css',
+            'alertify.core.css',
+            'alertify.default.css',
+            'dataTables.bootstrap.css',
+            'icheck.blue.css',
+            'AdminLTE.css',
+            'AdminLTE.all.skins.css',
+            'animate.css',
+            'asgard.css',
+        ],
+        'js' => [
+            'bootstrap.js',
+            'mousetrap.js',
+            'alertify.js',
+            'icheck.js',
+            'jquery.dataTables.js',
+            'dataTables.bootstrap.js',
+            'jquery.slug.js',
+            'keypressAction.js',
+            'app.js',
+            'main.js',
+            'sisyphus.js',
+        ],
+    ],
 ];
