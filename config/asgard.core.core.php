efc686d8bb482f46ac04b6f1d264615e582e5206
Nicolas Widart <n.widart@gmail.com>
2015-11-30 17:54:43 +0100

    Adding pace jquery plugin

--- config/asgard.core.core.php
+++ config/asgard.core.core.php
@@ -74,6 +74,7 @@ return [
         'selectize.css' => ['module' => 'core:vendor/selectize/dist/css/selectize.css'],
         'selectize-default.css' => ['module' => 'core:vendor/selectize/dist/css/selectize.default.css'],
         'animate.css' => ['theme' => 'vendor/animate.css/animate.min.css'],
+        'pace.css' => ['theme' => 'vendor/admin-lte/plugins/pace/pace.min.css'],
         // Javascript
         'bootstrap.js' => ['theme' => 'vendor/bootstrap/dist/js/bootstrap.min.js'],
         'mousetrap.js' => ['theme' => 'js/vendor/mousetrap.min.js'],
@@ -98,6 +99,7 @@ return [
         'sisyphus.js' => ['theme' => 'vendor/sisyphus/sisyphus.min.js'],
         'main.js' => ['theme' => 'js/main.js'],
         'chart.js' => ['theme' => 'vendor/admin-lte/plugins/chartjs/Chart.js'],
+        'pace.js' => ['theme' => 'vendor/admin-lte/plugins/pace/pace.min.js'],
     ],
 
     /*
@@ -117,6 +119,7 @@ return [
             'AdminLTE.css',
             'AdminLTE.all.skins.css',
             'animate.css',
+            'pace.css',
             'asgard.css',
         ],
         'js' => [
@@ -129,6 +132,7 @@ return [
             'jquery.slug.js',
             'keypressAction.js',
             'app.js',
+            'pace.js',
             'main.js',
             'sisyphus.js',
         ],
--- public/themes/adminlte/js/main.js
+++ public/themes/adminlte/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
     $('[data-slug="source"]').each(function(){
 	    $(this).slug();
 	});
+    $(document).ajaxStart(function() { Pace.restart(); });
 });
