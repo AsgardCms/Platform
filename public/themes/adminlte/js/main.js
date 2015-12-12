e30fe8762de59c4987164ecc0e453b35c24e9902
Nicolas Widart <n.widart@gmail.com>
2015-12-12 11:24:03 +0100

    New published main.js asset

--- public/themes/adminlte/js/main.js
+++ public/themes/adminlte/js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function () {
     $("form").sisyphus({locationBased: true, excludeFields: $('input[name="_token"]')});
+
     $('[data-slug="source"]').each(function(){
 	    $(this).slug();
 	});
+
     $(document).ajaxStart(function() { Pace.restart(); });
+
+    Mousetrap.bind('f1', function() {
+        window.open('https://asgardcms.com/docs', '_blank');
+    });
 });
