21ba87ae73ed2c3a563dc1e409b7e22a1a3898b6
Nicolas Widart <n.widart@gmail.com>
2016-07-16 12:45:03 +0200

    Publish tag configuration file

---
+++ config/asgard/tag/permissions.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'tag.tags' => [
+        'index' => 'tag::tags.list resource',
+        'create' => 'tag::tags.create resource',
+        'edit' => 'tag::tags.edit resource',
+        'destroy' => 'tag::tags.destroy resource',
+    ],
+];
