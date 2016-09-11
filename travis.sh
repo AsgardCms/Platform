7f26c62a0034254de98a661737bcd14aa3fb0f41
Nicolas Widart <n.widart@gmail.com>
2016-09-11 10:33:40 +0200

    Adding custom travis bash script to reset changes from repository after composer

--- travis.sh
+++ travis.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 
-travis_retry composer self-update
-travis_retry composer update ${COMPOSER_FLAGS} --no-interaction --prefer-dist
+# Loop over every module and theme to reset it to the original changes
+
+cd Modules
+for D in `find . -maxdepth 1 -mindepth 1 -type d`
+do
+    module=`echo "$D" | sed -e "s/^.\///"`
+    rm -rf $module
+    git reset HEAD $module
+    sleep .1
+    git checkout -- $module
+done
+
+cd ../Themes/
+for D in `find . -maxdepth 1 -mindepth 1 -type d`
+do
+    theme=`echo "$D" | sed -e "s/^.\///"`
+    rm -rf $theme
+    git reset HEAD $theme
+    sleep .1
+    git checkout -- $theme
+done
+
+
+
+
