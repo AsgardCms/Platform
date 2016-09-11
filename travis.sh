57cdcb4caabc98fd2c921e8c3decacc2934b89da
Nicolas Widart <n.widart@gmail.com>
2016-09-11 10:40:05 +0200

    Avoid looping over themes and modules

--- travis.sh
+++ travis.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-# Loop over every module and theme to reset it to the original changes
+# Reset Modules and Themes to their original state after composer has run
 
-cd Modules
-for D in `find . -maxdepth 1 -mindepth 1 -type d`
-do
-    module=`echo "$D" | sed -e "s/^.\///"`
-    rm -rf $module
-    git reset HEAD $module
-    sleep .1
-    git checkout -- $module
-done
-
-cd ../Themes/
-for D in `find . -maxdepth 1 -mindepth 1 -type d`
-do
-    theme=`echo "$D" | sed -e "s/^.\///"`
-    rm -rf $theme
-    git reset HEAD $theme
-    sleep .1
-    git checkout -- $theme
-done
-
-
-
-
+rm -rf Modules Themes
+git reset HEAD Modules
+git reset HEAD Themes
+git checkout -- Modules
+git checkout -- Themes
