81101e30435b8ac7bc82f7adc4074acd0eac05b4
Nicolas Widart <n.widart@gmail.com>
2016-09-13 20:22:18 +0200

    Adding a test for the UserRegistration class

---
+++ Modules/User/Tests/BaseUserTestCase.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Modules\User\Tests;
+
+use Illuminate\View\ViewServiceProvider;
+use Modules\Core\Providers\CoreServiceProvider;
+use Modules\User\Providers\UserServiceProvider;
+use Nwidart\Modules\LaravelModulesServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+abstract class BaseUserTestCase extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->resetDatabase();
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            ViewServiceProvider::class,
+            LaravelModulesServiceProvider::class,
+            CoreServiceProvider::class,
+            UserServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['path.base'] = __DIR__ . '/..';
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', array(
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ));
+        $app['config']->set('translatable.locales', ['en', 'fr']);
+        $app['config']->set('modules.paths.modules', __DIR__ . '/../Modules');
+    }
+
+    private function resetDatabase()
+    {
+        // Makes sure the migrations table is created
+        $this->artisan('migrate', [
+            '--database' => 'sqlite',
+        ]);
+        // We empty all tables
+        $this->artisan('migrate:reset', [
+            '--database' => 'sqlite',
+        ]);
+        // Migrate
+        $this->artisan('migrate', [
+            '--database' => 'sqlite',
+        ]);
+        $this->artisan('migrate');
+    }
+}
---
+++ Modules/User/Tests/UserRegistrationTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\User\Tests;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Modules\User\Repositories\RoleRepository;
+use Modules\User\Repositories\UserRepository;
+use Modules\User\Services\UserRegistration;
+
+class UserRegistrationTest extends BaseUserTestCase
+{
+    /**
+     * @var RoleRepository
+     */
+    private $role;
+    /**
+     * @var UserRepository
+     */
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->role = app(RoleRepository::class);
+        $this->user = app(UserRepository::class);
+    }
+
+    /** @test */
+    public function it_registers_a_new_user_with_user_role()
+    {
+        $this->createRole('User');
+
+        app(UserRegistration::class)->register([
+            'email' => 'n.widart@gmail.com',
+            'password' => 'demo1234',
+        ]);
+
+        $user = $this->user->find(1);
+
+        self::assertCount(1, $this->user->all());
+        self::assertEquals('n.widart@gmail.com', $user->email);
+        self::assertEquals('User', $user->roles->first()->name);
+    }
+
+    private function createRole($name)
+    {
+        return $this->role->create([
+            'name' => $name,
+            'slug' => str_slug($name),
+        ]);
+    }
+}
