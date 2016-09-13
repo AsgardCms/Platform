018570f434df29eb85ef80d926dc884f512fc6f0
Nicolas Widart <n.widart@gmail.com>
2016-09-13 20:15:33 +0200

    Adding the sentinel migrations to user module

---
+++ Modules/User/Database/Migrations/2014_07_02_230147_migration_cartalyst_sentinel.php
@@ -0,0 +1,124 @@
+<?php
+
+/**
+ * Part of the Sentinel package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.
+ *
+ * @package    Sentinel
+ * @version    2.0.12
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2015, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class MigrationCartalystSentinel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('code');
+            $table->boolean('completed')->default(0);
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+        });
+
+        Schema::create('persistences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('code');
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->unique('code');
+        });
+
+        Schema::create('reminders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('code');
+            $table->boolean('completed')->default(0);
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('slug');
+            $table->string('name');
+            $table->text('permissions')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->unique('slug');
+        });
+
+        Schema::create('role_users', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->nullableTimestamps();
+
+            $table->engine = 'InnoDB';
+            $table->primary(['user_id', 'role_id']);
+        });
+
+        Schema::create('throttle', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('type');
+            $table->string('ip')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->index('user_id');
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email');
+            $table->string('password');
+            $table->text('permissions')->nullable();
+            $table->timestamp('last_login')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+            $table->unique('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('activations');
+        Schema::drop('persistences');
+        Schema::drop('reminders');
+        Schema::drop('roles');
+        Schema::drop('role_users');
+        Schema::drop('throttle');
+        Schema::drop('users');
+    }
+}
