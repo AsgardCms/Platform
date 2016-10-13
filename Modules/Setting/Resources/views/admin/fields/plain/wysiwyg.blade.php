5cf30bafc6b067093393c286634d8392245853c3
Andrew Bekesh <arma@blackarma.com>
2016-10-13 12:45:10 +0300

    Additional check for null in database (#275)
    - Checking on field database plainvalue not to be null (string null still supported)
    - Change flag on SettingWasUpdated to false

--- Modules/Setting/Resources/views/admin/fields/plain/wysiwyg.blade.php
+++ Modules/Setting/Resources/views/admin/fields/plain/wysiwyg.blade.php
@@ -1,6 +1,6 @@
 <div class='form-group'>
     {!! Form::label($settingName, trans($moduleInfo['description'])) !!}
-    <?php if (isset($dbSettings[$settingName])): ?>
+    <?php if (isset($dbSettings[$settingName]) && $dbSettings[$settingName]->plainValue !== null): ?>
         {!! Form::textarea($settingName, old($settingName, $dbSettings[$settingName]->plainValue), ['class' => 'form-control ckeditor', 'placeholder' => trans($moduleInfo['description'])]) !!}
     <?php else: ?>
         {!! Form::textarea($settingName, old($settingName), ['class' => 'form-control ckeditor', 'placeholder' => trans($moduleInfo['description'])]) !!}
