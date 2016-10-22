b1b453d64ad9249521d56b97909b12505dfa95b3
oimken <oimken@gmail.com>
2016-10-22 20:04:21 +0800

    Simplified Chiniese add

---
+++ Modules/Translation/Resources/lang/menu/zh/menu-items.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'form' => [
+        'page' => '页面',
+        'module' => '模块',
+        'target' => '目标',
+        'same tab' => '当前窗口',
+        'new tab' => '新窗口',
+        'icon' => '图标',
+        'class' => 'CSS样式',
+        'parent menu item' => '上级菜单条目',
+    ],
+    'link-type' => [
+        'link type' => '链接类型',
+        'page' => '页面',
+        'internal' => '内部链接',
+        'external' => '外部链接',
+    ],
+    'list resource' => '菜单条目列表',
+    'create resource' => '创建菜单条目',
+    'edit resource' => '编辑菜单条目',
+    'destroy resource' => '删除菜单条目',
+];
