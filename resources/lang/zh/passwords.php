64b49f90199aa8c507ea4ce04b3026583d75ff13
oimken <oimken@gmail.com>
2016-10-22 17:53:34 +0800

    Simplified Chinese Added

---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码至少为6位字符并且与重复输入的一致.',
+    'reset' => '密码已重设!',
+    'sent' => '密码重设链接已通过 EMAIL 发送!',
+    'token' => '密码重设令牌无效.',
+    'user' => "无法找到与此 Email 关联的用户.",
+
+];
