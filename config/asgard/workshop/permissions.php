b9d45de2957de70362cf0f41a0db9bef3b9d9e04
Nicolas Widart <n.widart@gmail.com>
2017-01-09 10:21:42 +0100

    Updating published module configurations

--- config/asgard/workshop/permissions.php
+++ config/asgard/workshop/permissions.php
@@ -1,17 +1,20 @@
 <?php
 
 return [
+    'workshop.sidebar' => [
+        'group' => 'workshop::workshop.show sidebar group',
+    ],
     'workshop.modules' => [
         'index' => 'workshop::modules.list resource',
         'show' => 'workshop::modules.show resource',
         'update' => 'workshop::modules.update resource',
         'disable' => 'workshop::modules.disable resource',
         'enable' => 'workshop::modules.enable resource',
-        'publish' => 'workshop::modules.publish assets'
+        'publish' => 'workshop::modules.publish assets',
     ],
     'workshop.themes' => [
         'index' => 'workshop::themes.list resource',
         'show' => 'workshop::themes.show resource',
-        'publish' => 'workshop::themes.publish assets'
+        'publish' => 'workshop::themes.publish assets',
     ],
 ];
