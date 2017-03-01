76d1a4330639062f30b1183e161b286fc5a94a8d
Micheal Mand <micheal@kmdwebdesigns.com>
2017-03-01 13:41:34 -0700

    Fix missing namespace
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- Modules/User/Emails/ResetPasswordEmail.php
+++ Modules/User/Emails/ResetPasswordEmail.php
@@ -36,6 +36,6 @@ class ResetPasswordEmail extends Mailable implements ShouldQueue
     public function build()
     {
         return $this->view('user::emails.reminder')
-            ->subject(trans('reset password'));
+            ->subject(trans('user::messages.reset password'));
     }
 }
