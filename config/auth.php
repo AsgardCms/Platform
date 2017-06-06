12b65738e9213ec8e2a0d3e8734022c2dd62b6cf
Nicolas Widart <n.widart@gmail.com>
2017-06-06 21:37:00 +0200

    Specifying the asgard user model in case a package needs it.

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => Modules\User\Entities\Sentinel\User::class,
         ],
 
         // 'users' => [
