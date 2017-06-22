969a147700cea2721113b2208cf51dd745e14a31
Nicolas Widart <n.widart@gmail.com>
2017-06-22 14:51:13 +0200

    Fixing where clause whereKey to where('key',  in 5.4 this broke and isn't searching for the id column and not the key column.

--- Modules/Translation/Repositories/Eloquent/EloquentTranslationRepository.php
+++ Modules/Translation/Repositories/Eloquent/EloquentTranslationRepository.php
@@ -16,7 +16,6 @@ class EloquentTranslationRepository extends EloquentBaseRepository implements Tr
     public function findByKeyAndLocale($key, $locale = null)
     {
         $locale = $locale ?: app()->getLocale();
-
         $translation = $this->model->where('key', $key)->with('translations')->first();
         if ($translation && $translation->hasTranslation($locale)) {
             return $translation->translate($locale)->value;
