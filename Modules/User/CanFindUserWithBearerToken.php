c7d0da5f12538bde1e2ddda11ea90a2a47ad475c
Nicolas Widart <n.widart@gmail.com>
2017-06-29 16:45:07 +0200

    Create a helper trait to find a user by the given Bearer token

---
+++ Modules/User/CanFindUserWithBearerToken.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\User;
+
+use Modules\User\Entities\UserInterface;
+use Modules\User\Repositories\UserTokenRepository;
+
+trait CanFindUserWithBearerToken
+{
+    /**
+     * @param string $token
+     * @return UserInterface|null
+     */
+    public function findUserWithBearerToken($token)
+    {
+        $token = app(UserTokenRepository::class)->findByAttributes(['access_token' => $this->parseToken($token)]);
+
+        if ($token === null) {
+            return null;
+        }
+
+        return $token->user;
+    }
+
+    private function parseToken($token)
+    {
+        return str_replace('Bearer ', '', $token);
+    }
+}
