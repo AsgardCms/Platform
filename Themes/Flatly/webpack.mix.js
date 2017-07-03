523942f78f1d8ad06eb3b3eeb1bb8b94a6747a4f
Motch Julien <ju.motch@gmail.com>
2017-07-03 13:42:19 +0200

    Flatly with Laravel mix

--- Themes/Flatly/webpack.mix.js
+++ Themes/Flatly/webpack.mix.js
@@ -11,25 +11,25 @@ mix.less('resources/less/main.less', 'assets/css/main.css')
  * Concat scripts
  */
 mix.scripts([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-    'node_modules/prismjs/prism.js',
-    'resources/js/bootswatch.js'
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+  'node_modules/prismjs/prism.js',
+  'resources/js/bootswatch.js'
 ], 'assets/js/all.js');
 
 /**
  * Copy Font directory https://laravel.com/docs/5.4/mix#url-processing
  */
 mix.copy(
-    'fonts',
-    '../../public/fonts'
+  'fonts',
+  '../../public/fonts'
 );
 
 /**
  * Publishing the assets
  */
 mix.webpackConfig({
-   plugins: [
-   		    	new WebpackShellPlugin({onBuildEnd:['php ../../artisan stylist:publish ' + themeInfo.name]})
-   	]
+  plugins: [
+    new WebpackShellPlugin({onBuildEnd:['php ../../artisan stylist:publish ' + themeInfo.name]})
+  ]
 });
