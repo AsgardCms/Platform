b687f874e212b2579ff7b05b57fced8333277e95
Nicolas Widart <n.widart@gmail.com>
2017-07-12 21:47:59 +0200

    Create an EditorIsRendering Hook

---
+++ Modules/Core/Events/EditorIsRendering.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Modules\Core\Events;
+
+use Modules\Core\Foundation\Asset\Pipeline\AssetPipeline;
+
+class EditorIsRendering
+{
+    /**
+     * @var AssetPipeline
+     */
+    private $assetPipeline;
+    private $editorClass;
+    private $editorJsPartial;
+
+    public function __construct(AssetPipeline $assetPipeline)
+    {
+        $this->assetPipeline = $assetPipeline;
+    }
+
+    public function addJs($asset)
+    {
+        $this->assetPipeline->requireJs($asset);
+
+        return $this;
+    }
+
+    public function addCss($asset)
+    {
+        $this->assetPipeline->requireCss($asset);
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEditorClass()
+    {
+        return $this->editorClass;
+    }
+
+    /**
+     * @param mixed $editorClass
+     */
+    public function setEditorClass($editorClass)
+    {
+        $this->editorClass = $editorClass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEditorJsPartial()
+    {
+        return $this->editorJsPartial;
+    }
+
+    /**
+     * @param mixed $editorJsPartial
+     */
+    public function setEditorJsPartial($editorJsPartial)
+    {
+        $this->editorJsPartial = $editorJsPartial;
+    }
+}
