51ee8acb89802ef3e48a61357427e5bbabf921a9
Nicolas Widart <n.widart@gmail.com>
2017-07-15 00:25:09 +0200

    Moving css and js stacks outside the component. Making field and label name dynamic

--- Modules/Core/Resources/views/components/i18n/textarea.blade.php
+++ Modules/Core/Resources/views/components/i18n/textarea.blade.php
@@ -1,14 +1,6 @@
-@if ($editor->getEditorCssPartial() !== null)
-    @include($editor->getEditorCssPartial())
-@endif
-
-<div class='{{ $errors->has("{$lang}.body") ? ' has-error' : '' }}'>
-    {!! Form::label("{$lang}[body]", trans('page::pages.form.body')) !!}
-    <textarea class="{{ $editor->getEditorClass() }}" name="{{$lang}}[body]" rows="10" cols="80">{{ $slot }}</textarea>
-    {!! $errors->first("{$lang}.body", '<span class="help-block">:message</span>') !!}
+<div class='{{ $errors->has("{$lang}.{$fieldName}") ? ' has-error' : '' }}'>
+    {!! Form::label("{$lang}[{$fieldName}]", $labelName) !!}
+    <textarea class="{{ $editor->getEditorClass() }}" name="{{$lang}}[{$fieldName}]" rows="10" cols="80">{{ $slot }}</textarea>
+    {!! $errors->first("{$lang}.{$fieldName}", '<span class="help-block">:message</span>') !!}
 </div>
-
-@if ($editor->getEditorJsPartial() !== null)
-    @include($editor->getEditorJsPartial())
-@endif
 
