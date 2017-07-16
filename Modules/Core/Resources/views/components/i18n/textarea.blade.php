4f552936bd968cec85a265000ec1a39d74703462
Nicolas Widart <n.widart@gmail.com>
2017-07-16 14:20:14 +0200

    Fixing field name variable echo

--- Modules/Core/Resources/views/components/i18n/textarea.blade.php
+++ Modules/Core/Resources/views/components/i18n/textarea.blade.php
@@ -6,7 +6,7 @@
 
 <div class='{{ $errors->has("{$lang}.{$fieldName}") ? ' has-error' : '' }}'>
     {!! Form::label("{$lang}[{$fieldName}]", $labelName) !!}
-    <textarea class="{{ $editor->getEditorClass() }}" name="{{$lang}}[{$fieldName}]" rows="10" cols="80">{{ $slot }}</textarea>
+    <textarea class="{{ $editor->getEditorClass() }}" name="{{$lang}}[{{$fieldName}}]" rows="10" cols="80">{{ $slot }}</textarea>
     {!! $errors->first("{$lang}.{$fieldName}", '<span class="help-block">:message</span>') !!}
 </div>
 
