9b7077efc0c05643f4b458d26749a3ad23355543
Mehedi Hassan <MehediDracula@gmail.com>
2017-07-25 17:39:10 +0600

    Stop making DB queries on console mode

--- Modules/Core/Providers/AsgardServiceProvider.php
+++ Modules/Core/Providers/AsgardServiceProvider.php
@@ -12,7 +12,7 @@ class AsgardServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        if (class_exists(TranslationServiceProvider::class)) {
+        if ($this->app->runningInConsole() === false && class_exists(TranslationServiceProvider::class)) {
             $this->app->register(TranslationServiceProvider::class);
         }
 
