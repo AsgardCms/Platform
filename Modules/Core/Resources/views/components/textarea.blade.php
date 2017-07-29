d1191f3763a6d2e8f62f45c42af72c94079d257a
Micheal Mand <micheal@kmdwebdesigns.com>
2017-07-29 09:35:09 -0600

    Add missing `id` attribute to editor's textarea (#359)
    * Add missing `id` attribute to editor's textarea
    
    Without this, it is impossible to add custom config to a single CKEditor
     instance since `CKEDITOR.replace()` uses the `id` of the field, not the
     name.
    
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>
    
    * Fix `id` attribute for i18n editor
    
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- Modules/Core/Resources/views/components/textarea.blade.php
+++ Modules/Core/Resources/views/components/textarea.blade.php
@@ -6,7 +6,7 @@
 
 <div class='{{ $errors->has($fieldName) ? ' has-error' : '' }}'>
     {!! Form::label($fieldName, $labelName) !!}
-    <textarea class="{{ $editor->getEditorClass() }}" name="{{ $fieldName }}" rows="10" cols="80">{{ $slot }}</textarea>
+    <textarea class="{{ $editor->getEditorClass() }}" name="{{ $fieldName }}" id="{{ $fieldName }}" rows="10" cols="80">{{ $slot }}</textarea>
     {!! $errors->first($fieldName, '<span class="help-block">:message</span>') !!}
 </div>
 
