b38c257443a1feabdce8188f8ec9491dcaf7993c
Nicolas Widart <n.widart@gmail.com>
2017-08-01 11:49:56 +0200

    Added an AsgardCms class containing the current cms version

---
+++ Modules/Core/AsgardCms.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Core;
+
+class AsgardCms
+{
+    /**
+     * The AsgardCms version.
+     * @var string
+     */
+    const VERSION = '2.5.2';
+}
--- Modules/Core/Composers/ApplicationVersionViewComposer.php
+++ Modules/Core/Composers/ApplicationVersionViewComposer.php
@@ -2,63 +2,16 @@
 
 namespace Modules\Core\Composers;
 
-use Illuminate\Contracts\Cache\Repository;
-use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Modules\Core\AsgardCms;
 
 class ApplicationVersionViewComposer
 {
-    /**
-     * @var Filesystem
-     */
-    private $filesystem;
-    /**
-     * @var Repository
-     */
-    private $cache;
-    /**
-     * @var Request
-     */
-    private $request;
-
-    public function __construct(Filesystem $filesystem, Repository $cache, Request $request)
-    {
-        $this->filesystem = $filesystem;
-        $this->cache = $cache;
-        $this->request = $request;
-    }
-
     public function compose(View $view)
     {
         if (app('asgard.onBackend') === false) {
             return;
         }
-        $view->with('version', $this->getAppVersion());
-    }
-
-    /**
-     * @return string
-     */
-    private function getAppVersion()
-    {
-        $composerFile = $this->getComposerFile();
-
-        return isset($composerFile->version) ? $composerFile->version : '1.0';
-    }
-
-    /**
-     * Get the decoded contents from the main composer.json file
-     * @return object
-     */
-    private function getComposerFile()
-    {
-        $appName = str_slug(config('app.name'));
-
-        $composerFile = $this->cache->remember("app.version.$appName", 1440, function () {
-            return $this->filesystem->get('composer.json');
-        });
-
-        return json_decode($composerFile);
+        $view->with('version', AsgardCms::VERSION);
     }
 }
