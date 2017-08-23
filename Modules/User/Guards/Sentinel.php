7b870594f66045cbb5503dc674f636bb376bcba3
Nicolas Widart <n.widart@gmail.com>
2017-08-23 14:08:40 +0200

    Adding logout and loginUsingId methods on the sentinal guard

--- Modules/User/Guards/Sentinel.php
+++ Modules/User/Guards/Sentinel.php
@@ -80,8 +80,32 @@ class Sentinel implements LaravelGuard
         $this->setUser($user);
     }
 
+    /**
+     * @param array $credentials
+     * @param bool $remember
+     * @return bool
+     */
     public function attempt(array $credentials, $remember = false)
     {
         return SentinelFacade::authenticate($credentials, $remember);
+    }
+
+    /**
+     * @return bool
+     */
+    public function logout()
+    {
+        return SentinelFacade::logout();
+    }
+
+    /**
+     * @param int $userId
+     * @return bool
+     */
+    public function loginUsingId($userId)
+    {
+        $impersonatedUser = app(\Modules\User\Repositories\UserRepository::class)->find($userId);
+
+        return $this->login($impersonatedUser);
     }
 }
