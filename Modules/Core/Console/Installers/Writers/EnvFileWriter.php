55fffc414120ab4fdefe1d345a49347ca2d532c9
Nicolas Widart <n.widart@gmail.com>
2017-09-09 21:35:40 +0200

    Fixing order of what to search on .env

--- Modules/Core/Console/Installers/Writers/EnvFileWriter.php
+++ Modules/Core/Console/Installers/Writers/EnvFileWriter.php
@@ -16,8 +16,8 @@ class EnvFileWriter
      */
     protected $search = [
         'DB_CONNECTION=mysql',
-        'DB_PORT=3306',
         'DB_HOST=127.0.0.1',
+        'DB_PORT=3306',
         'DB_DATABASE=homestead',
         'DB_USERNAME=homestead',
         'DB_PASSWORD=secret',
