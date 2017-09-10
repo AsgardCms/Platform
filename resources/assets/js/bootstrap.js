fbf6f4d87f0707dd949f2378126c8b6491d6f4d2
Nicolas Widart <n.widart@gmail.com>
2017-09-10 16:34:06 +0200

    Don't include jquery

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -7,11 +7,11 @@ window._ = require('lodash');
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
+// try {
+//     window.$ = window.jQuery = require('jquery');
+//
+//     require('bootstrap-sass');
+// } catch (e) {}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
