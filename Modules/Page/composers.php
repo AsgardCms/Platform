db7776fe5377752dfb97ab28a842d91c7271927c
Nicolas Widart <n.widart@gmail.com>
2017-09-11 19:36:48 +0200

    Using ::class notation

--- Modules/Page/composers.php
+++ Modules/Page/composers.php
@@ -1,3 +1,3 @@
 <?php
 
-view()->composer(['page::admin.create', 'page::admin.edit'], 'Modules\Page\Composers\TemplateViewComposer');
+view()->composer(['page::admin.create', 'page::admin.edit'], \Modules\Page\Composers\TemplateViewComposer::class);
