d272f67d80b62fd48f3c794fe215402fbf6b181a
Nicolas Widart <n.widart@gmail.com>
2017-09-11 13:41:11 +0200

    Creating a page transformer class

---
+++ Modules/Page/Transformers/PageTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Page\Transformers;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class PageTransformer extends Resource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'created_at' => $this->created_at->format('d-m-Y'),
+            'urls' => [
+                'delete_url' => route('api.page.page.destroy', $this->id),
+                'edit_url' => route('admin.page.page.edit', $this->id),
+            ],
+        ];
+    }
+}
