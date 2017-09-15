55008316df3d32f81c3c526ac0945385d7ea76fe
Nicolas Widart <n.widart@gmail.com>
2017-09-15 11:14:10 +0200

    Setting up the vue router with basic page routes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,7 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
+import VueRouter from 'vue-router'
 import ElementUI from 'element-ui'
 import DataTables from 'vue-data-tables'
 import 'element-ui/lib/theme-default/index.css'
@@ -15,19 +9,22 @@ import locale from 'element-ui/lib/locale/lang/en'
 
 Vue.use(ElementUI, { locale });
 Vue.use(DataTables, { locale });
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.use(VueRouter);
 
 Vue.component('ckeditor', require('../../../Modules/Core/Assets/js/components/CkEditor.vue'));
 Vue.component('DeleteButton', require('../../../Modules/Core/Assets/js/components/DeleteComponent.vue'));
 Vue.component('TagsInput', require('../../../Modules/Tag/Assets/js/components/TagInput.vue'));
-Vue.component('PageTable', require('../../../Modules/Page/Assets/js/components/PageTable.vue'));
-Vue.component('PageForm', require('../../../Modules/Page/Assets/js/components/PageForm.vue'));
+import PageRoutes from '../../../Modules/Page/Assets/js/PageRoutes';
+
+const router = new VueRouter({
+    mode: 'history',
+    base: `${currentLocale}/backend`,
+    routes : [
+        ...PageRoutes,
+    ],
+});
 
 const app = new Vue({
     el: '#app',
+    router,
 });
