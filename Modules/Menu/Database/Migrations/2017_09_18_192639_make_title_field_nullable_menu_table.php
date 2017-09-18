cd507c4cb5ccd633c5158fdb36902bc57b719072
Nicolas Widart <n.widart@gmail.com>
2017-09-18 21:34:04 +0200

    - mysql 5.7 compatibility, making some additional columns nullable

---
+++ Modules/Menu/Database/Migrations/2017_09_18_192639_make_title_field_nullable_menu_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeTitleFieldNullableMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menu__menu_translations', function (Blueprint $table) {
+            $table->string('title')->nullable()->change();
+        });
+        Schema::table('menu__menuitems', function (Blueprint $table) {
+            $table->string('class')->nullable()->change();
+        });
+        Schema::table('menu__menuitem_translations', function (Blueprint $table) {
+            $table->string('title')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menu__menu_translations', function (Blueprint $table) {
+            $table->string('title')->nullable(false)->change();
+        });
+        Schema::table('menu__menuitems', function (Blueprint $table) {
+            $table->string('class')->nullable(false)->change();
+        });
+        Schema::table('menu__menuitem_translations', function (Blueprint $table) {
+            $table->string('title')->nullable(false)->change();
+        });
+    }
+}
