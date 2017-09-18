dc0e78d7e088071e9ff40e0aff5a149e1bbc5521
Nicolas Widart <n.widart@gmail.com>
2017-09-18 21:54:19 +0200

    Using translatable text

--- Themes/Flatly/views/partials/admin-bar.blade.php
+++ Themes/Flatly/views/partials/admin-bar.blade.php
@@ -25,9 +25,9 @@
         <div id="" class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
                 @if(isset($page))
-                    <li class=""><a href="{{ $page->getEditUrl() }}">Edit page</a></li>
+                    <li class=""><a href="{{ $page->getEditUrl() }}">{{ trans('page::pages.edit-page') }}</a></li>
                 @endif
-                <li class=""><a href="{{ route('dashboard.index') }}">Back to backend</a></li>
+                <li class=""><a href="{{ route('dashboard.index') }}">{{ trans('core::core.back to backend') }}</a></li>
             </ul>
         </div>
     </div>
