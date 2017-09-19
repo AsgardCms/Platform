b68e58c8b21c17aa0e6e43b3d17e4baf2e409995
Nicolas Widart <n.widart@gmail.com>
2017-09-19 21:01:48 +0200

    Creating a string helpers mixing file

---
+++ Modules/Core/Assets/js/mixins/StringHelpers.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+    export default {
+        methods: {
+            ucfirst(string) {
+                return string[0].toUpperCase() + string.substr(1);
+            },
+            ucwords(string) {
+                return string.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+            }
+        },
+    }
+</script>
