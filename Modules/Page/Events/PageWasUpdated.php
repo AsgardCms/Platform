2d970595be7ebae693edc3494c98d7ae06dcc4b9
Nicolas Widart <n.widart@gmail.com>
2017-09-19 21:48:18 +0200

    Implement the StoringMedia interface on page events

--- Modules/Page/Events/PageWasUpdated.php
+++ Modules/Page/Events/PageWasUpdated.php
@@ -2,9 +2,10 @@
 
 namespace Modules\Page\Events;
 
+use Modules\Media\Contracts\StoringMedia;
 use Modules\Page\Entities\Page;
 
-class PageWasUpdated
+class PageWasUpdated implements StoringMedia
 {
     /**
      * @var array
@@ -19,5 +20,23 @@ class PageWasUpdated
     {
         $this->data = $data;
         $this->page = $page;
+    }
+
+    /**
+     * Return the entity
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getEntity()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Return the ALL data sent
+     * @return array
+     */
+    public function getSubmissionData()
+    {
+        return $this->data;
     }
 }
