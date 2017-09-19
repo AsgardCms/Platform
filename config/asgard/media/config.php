ccf160014b73fb57654b6b16acb79dbdbcf3544c
Nicolas Widart <n.widart@gmail.com>
2017-09-19 13:24:24 +0200

    Allow pdfs to be uploaded by default too

--- config/asgard/media/config.php
+++ config/asgard/media/config.php
@@ -22,7 +22,7 @@ return [
     | Specify all the allowed file extensions a user can upload on the server
     |--------------------------------------------------------------------------
     */
-    'allowed-types' => '.jpg,.png',
+    'allowed-types' => '.jpg,.png,.pdf',
     /*
     |--------------------------------------------------------------------------
     | Determine the max file size upload rate
