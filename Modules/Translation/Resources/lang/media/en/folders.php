c73a624fd12a734af9130e0832eccc80ba0f3f5d
Nicolas Widart <n.widart@gmail.com>
2017-09-20 16:46:07 +0200

    Creating translations for folders

---
+++ Modules/Translation/Resources/lang/media/en/folders.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'list resource' => 'List folder',
+    'create resource' => 'Create folder',
+    'edit resource' => 'Edit folder',
+    'destroy resource' => 'Delete folder',
+];
