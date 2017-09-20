18aed5a796a36d7b385d287c79671b0eed70af4b
Nicolas Widart <n.widart@gmail.com>
2017-09-20 20:15:41 +0200

    Modifying files table to allow for folders

---
+++ Modules/Media/Database/Migrations/2017_09_20_144631_add_folders_columns_on_files_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFoldersColumnsOnFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media__files', function (Blueprint $table) {
+            $table->boolean('is_folder')->default(false)->after('id');
+
+            $table->string('path')->nullable()->change();
+            $table->string('extension')->nullable()->change();
+            $table->string('mimetype')->nullable()->change();
+            $table->string('filesize')->nullable()->change();
+            $table->string('folder_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media__files', function (Blueprint $table) {
+            $table->dropColumn('is_folder');
+
+            $table->string('path')->nullable(false)->change();
+            $table->string('extension')->nullable(false)->change();
+            $table->string('mimetype')->nullable(false)->change();
+            $table->string('filesize')->nullable(false)->change();
+            $table->string('folder_id')->nullable(false)->change();
+        });
+    }
+}
