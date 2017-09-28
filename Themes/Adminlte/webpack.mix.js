241410e4ed1b772aff281a7bbf72c4ab32c1c569
Nicolas Widart <n.widart@gmail.com>
2017-09-28 14:04:06 +0200

    Commenting out js copies

--- Themes/Adminlte/webpack.mix.js
+++ Themes/Adminlte/webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix').mix;
+let mix = require('laravel-mix');
 const WebpackShellPlugin = require('webpack-shell-plugin');
 const themeInfo = require('./theme.json');
 
@@ -23,19 +23,19 @@ mix.js('resources/assets/js/asgardcms.js', 'assets/js/asgardcms.js')
 /**
  * Copy node module
  */
-mix.copyDirectory('node_modules/admin-lte', 'assets/vendor/admin-lte');
-mix.copyDirectory('node_modules/animate.css', 'assets/vendor/animate.css');
-mix.copyDirectory('node_modules/bootstrap', 'assets/vendor/bootstrap');
-mix.copyDirectory('node_modules/clipboard', 'assets/vendor/clipboard');
-mix.copyDirectory('node_modules/datatables.net', 'assets/vendor/datatables.net');
-mix.copyDirectory('node_modules/datatables.net-bs', 'assets/vendor/datatables.net-bs');
-mix.copyDirectory('node_modules/font-awesome', 'assets/vendor/font-awesome');
-mix.copyDirectory('node_modules/gridstack', 'assets/vendor/gridstack');
-mix.copyDirectory('node_modules/icheck', 'assets/vendor/iCheck');
-mix.copyDirectory('node_modules/jquery', 'assets/vendor/jquery');
-mix.copyDirectory('node_modules/jquery-ui', 'assets/vendor/jquery-ui');
-mix.copyDirectory('node_modules/lodash', 'assets/vendor/lodash');
-mix.copyDirectory('node_modules/simplemde/src', 'assets/vendor/simplemde/src');
+// mix.copyDirectory('node_modules/admin-lte', 'assets/vendor/admin-lte');
+// mix.copyDirectory('node_modules/animate.css', 'assets/vendor/animate.css');
+// mix.copyDirectory('node_modules/bootstrap', 'assets/vendor/bootstrap');
+// mix.copyDirectory('node_modules/clipboard', 'assets/vendor/clipboard');
+// mix.copyDirectory('node_modules/datatables.net', 'assets/vendor/datatables.net');
+// mix.copyDirectory('node_modules/datatables.net-bs', 'assets/vendor/datatables.net-bs');
+// mix.copyDirectory('node_modules/font-awesome', 'assets/vendor/font-awesome');
+// mix.copyDirectory('node_modules/gridstack', 'assets/vendor/gridstack');
+// mix.copyDirectory('node_modules/icheck', 'assets/vendor/iCheck');
+// mix.copyDirectory('node_modules/jquery', 'assets/vendor/jquery');
+// mix.copyDirectory('node_modules/jquery-ui', 'assets/vendor/jquery-ui');
+// mix.copyDirectory('node_modules/lodash', 'assets/vendor/lodash');
+// mix.copyDirectory('node_modules/simplemde/src', 'assets/vendor/simplemde/src');
 
 /**
  * Publishing the assets
