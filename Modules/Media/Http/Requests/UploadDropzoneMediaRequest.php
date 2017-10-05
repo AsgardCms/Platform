6fbfdd6f6f417e652c9e8bae32f2ec1d9eb4eee1
Nicolas Widart <n.widart@gmail.com>
2017-10-05 20:55:40 +0200

    Adding extra size validation rule

--- Modules/Media/Http/Requests/UploadDropzoneMediaRequest.php
+++ Modules/Media/Http/Requests/UploadDropzoneMediaRequest.php
@@ -9,13 +9,40 @@ class UploadDropzoneMediaRequest extends FormRequest
 {
     public function rules()
     {
+        $extensions = 'mimes:' . str_replace('.', '', config('asgard.media.config.allowed-types'));
+        $maxFileSize = $this->getMaxFileSizeInKilobytes();
+
         return [
-            'file' => ['required', new MaxFolderSizeRule()],
+            'file' => [
+                'required',
+                new MaxFolderSizeRule(),
+                $extensions,
+                "size:$maxFileSize",
+            ],
+        ];
+    }
+
+    public function messages()
+    {
+        $size = $this->getMaxFileSize();
+
+        return [
+            'file.size' => "File is too large. Must be below {$size}MB."
         ];
     }
 
     public function authorize()
     {
         return true;
+    }
+
+    private function getMaxFileSizeInKilobytes()
+    {
+        return $this->getMaxFileSize() * 1000;
+    }
+
+    private function getMaxFileSize()
+    {
+        return config('asgard.media.config.max-file-size');
     }
 }
--- Modules/Media/Http/Requests/UploadMediaRequest.php
+++ Modules/Media/Http/Requests/UploadMediaRequest.php
@@ -10,14 +10,39 @@ class UploadMediaRequest extends FormRequest
     public function rules()
     {
         $extensions = 'mimes:' . str_replace('.', '', config('asgard.media.config.allowed-types'));
+        $maxFileSize = $this->getMaxFileSizeInKilobytes();
 
         return [
-            'file' => ['required', new MaxFolderSizeRule(), $extensions],
+            'file' => [
+                'required',
+                new MaxFolderSizeRule(),
+                $extensions,
+                "size:$maxFileSize",
+            ],
+        ];
+    }
+
+    public function messages()
+    {
+        $size = $this->getMaxFileSize();
+
+        return [
+            'file.size' => "File is too large. Must be below {$size}MB."
         ];
     }
 
     public function authorize()
     {
         return true;
+    }
+
+    private function getMaxFileSizeInKilobytes()
+    {
+        return $this->getMaxFileSize() * 1000;
+    }
+
+    private function getMaxFileSize()
+    {
+        return config('asgard.media.config.max-file-size');
     }
 }
