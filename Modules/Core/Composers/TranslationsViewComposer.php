58c41881791fa1348cad8e63a5afe70a6a3cccf3
Nicolas Widart <n.widart@gmail.com>
2017-10-08 11:50:55 +0200

    Adding folders translations

--- Modules/Core/Composers/TranslationsViewComposer.php
+++ Modules/Core/Composers/TranslationsViewComposer.php
@@ -12,6 +12,7 @@ class TranslationsViewComposer
             'page' => array_dot(trans('page::pages')),
             'core' => array_dot(trans('core::core')),
             'media' => array_dot(trans('media::media')),
+            'folders' => array_dot(trans('media::folders')),
         ]);
 
         $view->with(compact('staticTranslations'));
