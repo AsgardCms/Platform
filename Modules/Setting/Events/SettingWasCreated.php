ccfc29894284716c84c8439621a54f5c1b111a9f
Darron Park <darron1217@gmail.com>
2018-03-03 16:58:34 +0900

    Add view type media-single on Setting Module (to resolve logo uploading issue#474)

--- Modules/Setting/Events/SettingWasCreated.php
+++ Modules/Setting/Events/SettingWasCreated.php
@@ -2,17 +2,40 @@
 
 namespace Modules\Setting\Events;
 
+use Modules\Media\Contracts\StoringMedia;
 use Modules\Setting\Entities\Setting;
 
-class SettingWasCreated
+class SettingWasCreated implements StoringMedia
 {
     /**
      * @var Setting
      */
     public $setting;
 
-    public function __construct(Setting $setting)
+    /**
+     * @var array
+     */
+    public $data;
+
+    public function __construct(Setting $setting, $data)
     {
         $this->setting = $setting;
+        $this->data = $data;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getEntity()
+    {
+        return $this->setting;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSubmissionData()
+    {
+        return $this->data;
     }
 }
