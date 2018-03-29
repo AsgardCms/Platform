ed2378d777a038a28fb2eb91cfd676cec2b83ee1
Mariusz Fik <mariusz@fidano.pl>
2018-03-29 23:12:15 +0200

    Add polish translation for Media modules.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ Modules/Translation/Resources/lang/media/pl/media.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'title' => [
+        'media' => 'Media',
+        'edit media' => 'Edycja mediów',
+    ],
+    'breadcrumb' => [
+        'media' => 'Media',
+    ],
+    'table' => [
+        'filename' => 'Nazwa pliku',
+        'width' => 'Szerokość',
+        'height' => 'Wysokość',
+    ],
+    'form' => [
+        'alt_attribute' => 'Atrybut alt',
+        'description' => 'Opis',
+        'keywords' => 'Słowa kluczowe',
+    ],
+    'validation' => [
+        'max_size' => 'Osiągnięto maksymalny rozmiar (:size) katalogu w katalogu Media.',
+    ],
+    'file-sizes' => [
+        'B' => 'Bajty',
+        'KB' => 'Kb',
+        'MB' => 'Mb',
+        'GB' => 'Gb',
+        'TB' => 'Tb',
+    ],
+    'choose file' => 'Wybierz plik',
+    'insert' => 'Wstaw plik',
+    'file picker' => 'Wybieranie pliku',
+    'Browse' => 'Przeglądaj...',
+    'upload file' => 'Prześlij plik',
+    'remove media' => 'Usuń obraz',
+    'list resource' => 'Wyświetlanie',
+    'create resource' => 'Tworzenie',
+    'edit resource' => 'Edycja',
+    'destroy resource' => 'Usuwanie',
+    'file too large' => 'Plik jest za duży. Rozmiar nie może przekraczać :size MB.',
+    'some files not moved' => 'Niektóre z plików nie zostały przeniesione',
+    'files moved successfully' => 'Pliki przeniesione pomyślnie',
+];
