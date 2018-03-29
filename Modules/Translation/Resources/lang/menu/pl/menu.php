44f621163b2dc0e356773b7c78bd2cf3206f2503
Mariusz Fik <mariusz@fidano.pl>
2018-03-29 23:41:42 +0200

    Add polish translation for Menu module.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ Modules/Translation/Resources/lang/menu/pl/menu.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'title'  => 'Menu',
+    'titles' => [
+        'menu' => 'Zarządzanie menu',
+        'create menu' => 'Tworzenie menu',
+        'edit menu' => 'Edycja menu',
+        'create menu item' => 'Tworzenie elementu menu',
+        'edit menu item' => 'Edycja elementu menu',
+    ],
+    'breadcrumb' => [
+        'menu' => 'Zarządzanie menu',
+        'create menu' => 'Tworzenie menu',
+        'edit menu' => 'Edycja menu',
+        'create menu item' => 'Tworzenie elementu menu',
+        'edit menu item' => 'Edycja elementu menu',
+    ],
+    'button' => [
+        'create menu item' => 'Utwórz element menu',
+        'create menu' => 'Utwórz menu',
+    ],
+    'table' => [
+        'name' => 'Nazwa',
+        'title' => 'Tytuł',
+    ],
+    'form' => [
+        'title' => 'Tytuł',
+        'name' => 'Nazwa',
+        'status' => 'Online',
+        'uri' => 'URI',
+        'url' => 'URL',
+        'primary' => 'Menu główne (używane jako nawigacja na stronie głównej)',
+    ],
+    'navigation' => [
+        'back to index' => 'Powrót do zarządzania menu',
+    ],
+    'list resource' => 'Wyświetlanie',
+    'create resource' => 'Tworzenie',
+    'edit resource' => 'Edycja',
+    'destroy resource' => 'Usuwanie',
+];
---
+++ Modules/Translation/Resources/lang/menu/pl/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'only one primary menu' => 'Tylko jedno menu może być menu głównym',
+    'name is required' => 'Nazwa jest wymagana',
+];
