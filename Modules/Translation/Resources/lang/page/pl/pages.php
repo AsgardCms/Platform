7b0816785af85ccda0a8c24dee347d997a1d1b72
Mariusz Fik <mariusz@fidano.pl>
2018-03-31 00:00:47 +0200

    Update polish translation for Pages module.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

--- Modules/Translation/Resources/lang/page/pl/pages.php
+++ Modules/Translation/Resources/lang/page/pl/pages.php
@@ -2,23 +2,37 @@
 
 return [
     'pages' => 'Strony',
-    'create page' => 'Utwórz stronę',
-    'edit page' => 'Edytuj stronę',
-    'name' => 'Nazwa',
+    'create page' => 'Tworzenie strony',
+    'edit page' => 'Edycja strony',
+
     'slug' => 'Slug',
     'title' => 'Tytuł',
-    'meta_title' => 'Meta tytuł',
-    'meta_description' => 'Meta opis',
-    'og_title' => 'Facebook tytuł',
-    'og_description' => 'Facebook opis',
-    'og_type' => 'Facebook typ',
-    'template' => 'Szablon strony',
-    'is homepage' => 'Strona domowa ?',
+
+    'meta_data' => 'Metadane',
+    'meta_title' => 'Tytuł metadanych',
+    'meta_description' => 'Opis metadanych',
+    'facebook_data' => 'Facebook',
+    'og_title' => 'Tytuł OpenGraph',
+    'og_description' => 'Opis OpenGraph',
+    'og_type' => 'Rodzaj OpenGraph',
+    'template' => 'Szablon',
+    'is homepage' => 'Strona główna?',
     'body' => 'Treść',
+    'status' => 'Status',
+    'pages were updated' => 'Zaktualizowano stronę',
+
+    'back to index' => 'Powrót do zarządzania stronami',
+    'list resource' => 'Wyświetlanie',
+    'create resource' => 'Tworzenie',
+    'edit resource' => 'Edycja',
+    'destroy resource' => 'Usuwanie',
+
+    'view-page' => 'Podgląd strony',
+    'edit-page' => 'Edytuj stronę',
     'validation' => [
         'attributes' => [
-            'title' => 'title',
-            'body' => 'body',
+            'title' => 'tytuł',
+            'body' => 'treść',
         ],
     ],
     'facebook-types' => [
@@ -26,5 +40,4 @@ return [
         'product' => 'Produkt',
         'article' => 'Artykuł',
     ],
-    'back to index' => 'Wróć do indeksu stron',
 ];
