57ed45100bd7c38004241d88249241c167449b28
KaMe <julioramon767@gmail.com>
2018-04-05 11:31:23 -0400

    Update media spanish translation 3.5.2
    Update media spanish translation 3.5.2

--- Modules/Translation/Resources/lang/media/es/messages.php
+++ Modules/Translation/Resources/lang/media/es/messages.php
@@ -3,4 +3,7 @@
 return [
     'file updated' => 'Archivo actualizado',
     'file deleted' => 'Archivo eliminado',
+    'folder updated' => 'Carpeta actualizada',
+    'folder deleted' => 'Carpeta eliminada',
+    'selected items deleted' => 'Los Archivo(s) selecionados fueron eliminado',
 ];
