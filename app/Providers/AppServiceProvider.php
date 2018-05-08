070eb0b3ddfeb64e7202cfc6be0e1a1234a04619
KaMe <julioramon767@gmail.com>
2018-05-08 18:06:40 -0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        Carbon\Carbon::setLocale(config('app.locale'));
+        Carbon::setLocale(config('app.locale'));
         Carbon::serializeUsing(function ($carbon) {
             return $carbon->format('d/m/y H:i:s');
         });
