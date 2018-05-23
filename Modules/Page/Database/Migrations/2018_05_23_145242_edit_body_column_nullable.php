e1cff91038047b18b00e554c9cfec35b1c2a25e1
KaMe <julioramon767@gmail.com>
2018-05-23 11:12:48 -0400

    Update 2018_05_23_145242_edit_body_column_nullable.php
    Fix a typo sorry.

--- Modules/Page/Database/Migrations/2018_05_23_145242_edit_body_column_nullable.php
+++ Modules/Page/Database/Migrations/2018_05_23_145242_edit_body_column_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditBodyColumnNulleable extends Migration
+class EditBodyColumnNullable extends Migration
 {
     /**
      * Run the migrations.
