d9261e49fc22e4b9dd4604b47cee49847354f401
Pörös Dávid <davidporos92@gmail.com>
2018-06-20 18:38:58 +0200

    add hungarian translation for core

---
+++ Modules/Translation/Resources/lang/core/hu/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'site-name' => 'Weboldal neve',
+    'site-name-mini' => 'Weboldal rövid neve',
+    'site-description' => 'Weboldal leírása',
+    'template' => 'Publikus oldal sablonja',
+    'analytics-script' => 'Analytics Script',
+    'locales' => 'Weboldal nyelvei',
+];
