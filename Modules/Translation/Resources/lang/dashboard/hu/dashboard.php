5c15e15d5aefd97cd3aac949f215fefa03c419f6
Pörös Dávid <davidporos92@gmail.com>
2018-06-20 18:44:44 +0200

    add hungarian translation for dashboard

---
+++ Modules/Translation/Resources/lang/dashboard/hu/dashboard.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'name' => 'Vezérlőpult',
+    'edit grid' => 'Rács szerkesztése',
+    'reset grid' => 'Rács visszaállítása',
+    'save grid' => 'Rács mentése',
+    'add widget' => 'Widget hozzáadása',
+    'add widget to dashboard' => 'Widget elhelyezése a rácson',
+    'reset not needed' => 'A vezérlőpulton nincs szükség visszaállításra',
+    'dashboard reset' => 'A vezérlőpult visszaállítva',
+    'list resource' => 'Vezérlőpult mutatása',
+    'edit resource' => 'Vezérlőpult szerkesztése',
+    'reset resource' => 'Vezérlőpult visszaállítása',
+    'welcome-title' => 'Üdvőzlő cím',
+    'welcome-description' => 'Üdvözlő leírás',
+    'welcome-enabled' => 'Üdvözlő doboz engedélyezése?',
+    'configure your website' => 'Weboldal beállítása',
+    'add pages' => 'Oldalak hozzáadása',
+    'add menus' => 'Menük hozzadása',
+];
