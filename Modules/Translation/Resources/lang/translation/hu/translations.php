a9d59b3f931f2e9150c43988dad9eee668c23296
Pörös Dávid <davidporos92@gmail.com>
2018-06-20 19:26:33 +0200

    add hungarian translation for translation

---
+++ Modules/Translation/Resources/lang/translation/hu/translations.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'title' => [
+        'translations' => 'Fordítások',
+        'edit translation' => 'Fordítás szerkesztése',
+    ],
+    'button' => [
+        'create translation' => 'Fordítás létrehozása',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+    'Clear translation cache' => 'Fordítások cache ürítése',
+    'Export' => 'Export',
+    'Import' => 'Import',
+    'Import csv translations file' => 'Fordítások importálása CSV-ből',
+    'only for developers' => 'Git kezelés szükséges!',
+    'Translations exported' => 'Fordítások exportálva.',
+    'Translations imported' => 'Fordítások importálva.',
+    'select CSV file' => 'CSV fájl kiválasztása...',
+    'csv only allowed' => 'Kérlek csak CSV fájlt tölts fel.',
+    'time' => 'Időpont',
+    'author' => 'Szerző',
+    'history' => 'Előzmények',
+    'No Revisions yet' => 'Még nincsenek módosítások',
+    'event' => 'Esemény',
+    'created' => 'Létrehozva',
+    'edited' => 'Szerkesztve',
+    'revert history' => 'Módosítás történet',
+    'list resource' => 'Fordítások listázása',
+    'edit resource' => 'Fordítások szerkesztése',
+    'import resource' => 'Fordítások importálása',
+    'export resource' => 'Fordítások exportálása',
+];
