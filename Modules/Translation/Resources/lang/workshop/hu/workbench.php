6c610d836e87764193ce9941c844260cb5bc2f98
Pörös Dávid <davidporos92@gmail.com>
2018-06-20 19:56:25 +0200

    add hungarian translation for workshop

---
+++ Modules/Translation/Resources/lang/workshop/hu/workbench.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'title' => 'Modul segítők',
+    'subtitle' => [
+        'generate new module' => 'Új modul generálása',
+        'install new module by vendor name' => 'Modul telepítése szerző/név alapján',
+    ],
+    'tab' => [
+        'generators' => 'Generátorok',
+        'migrations' => 'Migrációk',
+        'seeds' => 'Seeds',
+    ],
+    'form' => [
+        'module name' => 'Modul neve',
+        'vendor name of the module' => 'Szerzője/neve a modulnak',
+        'install as subtree' => 'Telepítés almodulként?',
+    ],
+    'button' => [
+        'generate new module' => 'Új modul generálása',
+        'install new module' => 'Új modul telepítése',
+        'migrate' => 'Migrálás',
+        'seed' => 'Seed',
+    ],
+];
