080b0f7575d94ffe1e68dd565ce1ec04d8f5db8b
Pörös Dávid <davidporos92@gmail.com>
2018-06-20 19:47:50 +0200

    add hungarian translation for user

---
+++ Modules/Translation/Resources/lang/user/hu/roles.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'new-role' => 'Új szerepkör',
+    'title' => [
+        'roles' => 'Szerepkörök',
+        'edit' => 'Szerepkör szerkesztése',
+        'users-with-roles' => 'Felhasználók ezzel a szerepkörrel',
+    ],
+    'breadcrumb' => [
+        'roles' => 'Szerepkörök',
+        'new' => 'Új',
+        'edit' => 'Szerepkör szerkesztése',
+    ],
+    'table' => [
+        'name' => 'Név',
+        'slug' => 'Slug',
+    ],
+    'tabs' => [
+        'data' => 'Adat',
+        'permissions' => 'Engedélyek',
+    ],
+    'form' => [
+        'name' => 'Szerepkör neve',
+        'slug' => 'Szerepkör slug',
+    ],
+    'navigation' => [
+        'back to index' => 'Vissza a szerepkörökhöz',
+    ],
+    'select all' => 'Összes kiválasztása',
+    'deselect all' => 'Kiválasztások visszavonása',
+    'swap' => 'Felcserél',
+    'allow all' => 'Össze engedélyezése',
+    'deny all' => 'Összes tiltása',
+    'inherit all' => 'Összes örököltetése',
+    'allow' => 'Engedélyez',
+    'deny' => 'Tilt',
+    'inherit' => 'Örököltetés a szerepkörből',
+    'list resource' => 'Szerepkörök listázása',
+    'create resource' => 'Szerepkör létrehozása',
+    'edit resource' => 'Szerepkör szerkesztése',
+    'destroy resource' => 'Szerepkör törlése',
+];
