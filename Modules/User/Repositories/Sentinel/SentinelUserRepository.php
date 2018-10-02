9b48bb23dd9124dd8d847ad39189778fb572f610
Micheal Mand <micheal@kmdwebdesigns.com>
2018-10-02 13:19:37 -0600

    Fix #583: `activated` vs `is_activated`
    Thank you to @chiragpipariya for his initial fix in #586.

--- Modules/User/Repositories/Sentinel/SentinelUserRepository.php
+++ Modules/User/Repositories/Sentinel/SentinelUserRepository.php
@@ -260,7 +260,7 @@ class SentinelUserRepository implements UserRepository
      */
     private function checkForManualActivation($user, array &$data)
     {
-        if (Activation::completed($user) && !$data['activated']) {
+        if (Activation::completed($user) && !$data['is_activated']) {
             return Activation::remove($user);
         }
 
