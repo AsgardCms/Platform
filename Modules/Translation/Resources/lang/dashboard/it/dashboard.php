c088aadf761a997c9ec3fea4a0077768abdfb71e
Christian Giupponi <c.giupponi@zerouno.io>
2018-10-08 10:00:15 +0200

    Add some fixes for italian translations

--- Modules/Translation/Resources/lang/dashboard/it/dashboard.php
+++ Modules/Translation/Resources/lang/dashboard/it/dashboard.php
@@ -1,11 +1,22 @@
 <?php
 
+
 return [
     'name' => 'Dashboard',
     'edit grid' => 'Modifica griglia',
     'reset grid' => 'Resetta griglia',
+    'save grid' => 'Salva griglia',
     'add widget' => 'Aggiungi widget',
     'add widget to dashboard' => 'Aggiungi widget alla dashboard',
     'reset not needed' => 'La dashboard non ha bisogno di reset',
     'dashboard reset' => 'La dashboard è stata resettata',
+    'list resource' => 'View dashboard',
+    'edit resource' => 'Aggiorna dashboard',
+    'reset resource' => 'Ripristina dashboard',
+    'welcome-title' => 'Titolo di benvenuto',
+    'welcome-description' => 'Descrizione di benvenuto',
+    'welcome-enabled' => 'Abilitare il box di benvenuto?',
+    'configure your website' => 'Configura il sito',
+    'add pages' => 'Aggiungi pagine',
+    'add menus' => 'aggiungi menu',
 ];
