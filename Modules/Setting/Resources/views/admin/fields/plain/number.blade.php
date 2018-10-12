2e4c5b95015403a211094d397197651f66417ec5
Christian Giupponi <c.giupponi@zerouno.io>
2018-10-12 10:47:57 +0200

    Add possibility to define some options on number field

--- Modules/Setting/Resources/views/admin/fields/plain/number.blade.php
+++ Modules/Setting/Resources/views/admin/fields/plain/number.blade.php
@@ -1,9 +1,16 @@
-<?php $defaultValue = isset($moduleInfo['default']) ? $moduleInfo['default']: ''; ?>
+<?php
+    $defaultValue = isset($moduleInfo['default']) ? $moduleInfo['default']: '';
+    $defaultOptions = [
+        'class' => 'form-control',
+        'placeholder' => trans($moduleInfo['description'])
+    ];
+    $options = array_merge($defaultOptions, isset($moduleInfo['options']) ? $moduleInfo['options'] : []);
+?>
 <div class='form-group'>
     {!! Form::label($settingName, trans($moduleInfo['description'])) !!}
     <?php if (isset($dbSettings[$settingName]) && $dbSettings[$settingName]->plainValue !== null): ?>
-        {!! Form::number($settingName, old($settingName, $dbSettings[$settingName]->plainValue) ?: $defaultValue, ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        {!! Form::number($settingName, old($settingName, $dbSettings[$settingName]->plainValue) ?: $defaultValue, $options) !!}
     <?php else: ?>
-        {!! Form::number($settingName, old($settingName, $defaultValue), ['class' => 'form-control', 'placeholder' => trans($moduleInfo['description'])]) !!}
+        {!! Form::number($settingName, old($settingName, $defaultValue), $options) !!}
     <?php endif; ?>
 </div>
