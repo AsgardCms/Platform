61875d97ba7c7daea34b020f3ad61fbf6fdc8493
Christian Giupponi <c.giupponi@zerouno.io>
2018-10-23 11:28:40 +0200

    Possibile fix for alternate variable not set in page

--- Modules/Core/Http/Controllers/BasePublicController.php
+++ Modules/Core/Http/Controllers/BasePublicController.php
@@ -13,10 +13,25 @@ abstract class BasePublicController extends Controller
      */
     protected $auth;
     public $locale;
+    public $alternateUrls = [];
 
     public function __construct()
     {
         $this->locale = App::getLocale();
         $this->auth = app(Authentication::class);
+        view()->share('alternate', $this->alternateUrls);
     }
+
+    /**
+     * Add alternate URLs to main array and inject it to the page
+     *
+     * @param array $alternateUrls
+     * @return void
+     */
+    protected function addAlternateUrls(array $alternateUrls)
+    {
+        $this->alternateUrls = array_merge($this->alternateUrls, $alternateUrls);
+        view()->share('alternate', $this->alternateUrls);
+    }
+}
 }
