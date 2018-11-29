a3829054a75f4ff0b06ef5f7368164ee273a3933
Micheal Mand <micheal@kmdwebdesigns.com>
2018-11-29 12:42:17 -0700

    Some (opinionated) ESLint overrides
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,26 @@ module.exports = {
         'no-undef': 'off',
         'max-len': 'off',
         'no-console': 'off',
+        'one-var': ['error', { 'var': 'consecutive', 'let': 'consecutive', 'const': 'consecutive' }],
+        'no-plusplus': ['error', { 'allowForLoopAfterthoughts': true }],
+        'quote-props': ['error', 'as-needed', { 'unnecessary': false }],
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': ['error', {
+            'singleline': 4,
+            'multiline': {
+                'max': 1,
+                'allowFirstLine': false,
+            },
+        }],
+        'vue/html-self-closing': ['error', {
+            'html': {
+                'void': 'never',
+                'normal': 'any',
+                'component': 'any',
+            },
+            'svg': 'any',
+            'math': 'always',
+        }],
     },
     overrides: [
         {
