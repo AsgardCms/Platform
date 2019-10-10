267c692112c4842146f08f4bd9cc43527a5db4a8
Christian Giupponi <c.giupponi@zerouno.io>
2019-10-10 12:08:54 +0200

    Fire event when user logged in

---
+++ Modules/User/Events/UserLoggedIn.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\User\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserLoggedIn
+{
+    use Dispatchable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+}
