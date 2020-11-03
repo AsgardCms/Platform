d5a4dceee3e73b8f2fb413b31e3e074b7df776f5
Laravel Shift <shift@laravelshift.com>
2020-11-03 22:07:54 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them. This allows you to review the commit
    diff for once for customizations when you are done Shifting.
    
    Moving forward, consider using ENV variables or create a separate
    config file to allow the core config files to remain as default
    as possible.

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,7 +63,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL') . '/storage',
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
 
@@ -82,6 +82,21 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the symbolic links that will be created when the
+    | `storage:link` Artisan command is executed. The array keys should be
+    | the locations of the links and the values should be their targets.
+    |
+    */
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
     ],
 
 ];
