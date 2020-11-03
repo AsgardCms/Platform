35a84143013c7be094673b3de18b01de8c3d4c95
Laravel Shift <shift@laravelshift.com>
2020-11-03 22:07:54 +0000

    Shift config files

--- config/cache.php
+++ config/cache.php
@@ -39,6 +39,7 @@ return [
 
         'array' => [
             'driver' => 'array',
+            'serialize' => false,
         ],
 
         'database' => [
@@ -98,6 +99,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
 
 ];
